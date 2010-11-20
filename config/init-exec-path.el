c3180e59d4df797cec844b742953290427f52b1b
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2010-11-20 23:48:46 +0900

    exec-path: removing non related setup

--- config/init-exec-path.el
+++ config/init-exec-path.el
@@ -3,13 +3,4 @@
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 
-(when *is-a-mac*
-  (eval-after-load "woman"
-    '(setq woman-manpath (append (list "/opt/local/man") woman-manpath)))
-
-  ;; When started from Emacs.app or similar, ensure $PATH
-  ;; is the same the user would see in Terminal.app
-  (if window-system (set-exec-path-from-shell-PATH)))
-
-
 (provide 'init-exec-path)
