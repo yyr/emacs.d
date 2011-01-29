a9c91d1df748bc189d60f8e6bca41b7bbedc39f4
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2011-01-29 23:45:29 +0900

    add: init-yaoddmuse init-diminish

---
+++ config/init-diminish.el
@@ -0,0 +1,13 @@
+;;; init-diminish.el
+;;
+;; Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+;; Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+;;
+
+(require 'diminish)
+
+(diminish 'paredit-mode "Par")
+(diminish 'eldoc-mode "ElD")
+
+(provide 'init-diminish)
+;;; init-diminish.el ends here
---
+++ config/init-yaoddmuse.el
@@ -0,0 +1,13 @@
+;;; init-yaoddmuse.el
+;;
+;; Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+;; Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+;;
+
+(require 'yaoddmuse)
+
+(yaoddmuse-update-pagename nil)
+
+(provide 'init-yaoddmuse)
+
+;;; init-yaoddmuse.el ends here
