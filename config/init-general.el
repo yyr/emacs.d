423e3d8e4e98d85a99f42fdd36db9b7cee65847b
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2011-01-30 01:44:46 +0900

    general: added few from Andy Stewart init-misc (emacswiki.org)

--- config/init-general.el
+++ config/init-general.el
@@ -1,5 +1,29 @@
 ;;; General
 
+;;; ### Coding ###
+;; I dont what this means. but looks imp
+(setq default-buffer-file-coding-system 'utf-8-unix)            
+(setq default-file-name-coding-system 'utf-8-unix)              
+(setq default-keyboard-coding-system 'utf-8-unix)               
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix)) 
+(setq default-sendmail-coding-system 'utf-8-unix)               
+(setq default-terminal-coding-system 'utf-8-unix)               
+
+;;; ### Advice ###
+;;; yank with indent
+(defadvice yank (after indent-region activate)
+  "To make yank content indent automatically."
+  (if (member major-mode '(emacs-lisp-mode
+                           scheme-mode
+                           lisp-mode
+                           lisp-interaction-mode
+                           c-mode
+                           c++-mode
+                           objc-mode
+                           latex-mode
+                           plain-tex-mode))
+      (indent-region (region-beginning) (region-end) nil)))
+
 ;; stop welcome screen
 (custom-set-variables
  '(inhibit-startup-screen t))
--- config/init-yaoddmuse.el
+++ config/init-yaoddmuse.el
@@ -6,6 +6,12 @@
 
 (require 'yaoddmuse)
 
+
+(setq yaoddmuse-browse-function 'yaoddmuse-browse-page-in-w3m)  
+(setq yaoddmuse-notify-function 'yaoddmuse-notify-popup-window) 
+(setq yaoddmuse-wikis                                           
+      '(("EmacsWiki" "http://www.emacswiki.org/cgi-bin/emacs" utf-8 "uihnscuskc=1;")))
+
 (provide 'init-yaoddmuse)
 
 ;;; init-yaoddmuse.el ends here
