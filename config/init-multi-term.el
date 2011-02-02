b30d792b7f290878aa2967331c4ce99e0f5f838c
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2011-02-03 03:20:33 +0900

    add: init-multi-term.el to repo

---
+++ config/init-multi-term.el
@@ -0,0 +1,13 @@
+;;; init-multi-term.el
+;;
+;; Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+;; Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+;;
+
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
+
+
+(provide 'init-multi-term)
+
+;;; init-multi-term.el ends here
