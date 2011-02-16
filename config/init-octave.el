fe0e4e5acf7c42a3b8f937e7844df36599552334
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2011-02-16 17:35:31 +0900

    octave-mode and ac for octave mode setup

--- config/init-octave.el
+++ config/init-octave.el
@@ -1,19 +1,20 @@
-;; -----------------------------------------------------------------------------
-;; OCTAVE mode
-;; -----------------------------------------------------------------------------
+;;;  Octave mode
 
 ;; auto load 
 ;;(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
 
 ;; using Octave mode for all `.m' files
-(autoload 'octave-mode "octave-mode" nil t)
+(autoload 'octave-mode "octave-mod" nil t)
 (setq auto-mode-alist 
       (cons '("\\.m$" . octave-mode) auto-mode-alist))
 
-;; turn on abbrev and auto-fill
+;;; auto-complete 
 (add-hook 'octave-mode-hook
-	    (lambda ()
-	      (auto-fill-mode 1)))
+	  (lambda ()
+	    (require 'auto-complete-octave)))
+(setq octave-completion-alist
+      (nconc octave-keywords octave-completion-alist))
 
 
 (provide 'init-octave)
+;;; init-octave.el ends here
