ff9cd9a5d857df5b7e74c2a0f03a3d89d27e205c
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2011-03-01 17:25:02 +0900

    eldoc: dont show up in org-mode

--- config/init-eldoc.el
+++ config/init-eldoc.el
@@ -9,7 +9,7 @@
                'message-mode-hook
                'Info-mode-hook
                'erc-mode-hook
-               'org-mode-hook
+;;               'org-mode-hook
                ))
   (add-hook hook 'turn-on-eldoc-mode))
 ;(setq eldoc-argument-case 'eldoc-argument-list)
