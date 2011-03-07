384da3fdb232d6551135af6db6f8917236eef82d
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-03-08 01:39:33 +0900

    new:init-sass-mode.el

---
+++ config/init-sass-mode.el
@@ -0,0 +1,10 @@
+;;; init-sass-mode.el
+;;
+;; Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+;; Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+;;
+
+(add-hook 'sass-mode-hook 'ac-css-mode-setup)
+
+(provide 'init-sass-mode)
+;;; init-sass-mode.el ends here
