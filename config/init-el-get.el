30fb79d5b24628e9fea5f2bce7fd1b1a8f6667e1
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-03-15 07:45:27 +0900

    el-get: cleaned init-el-get.el

--- config/init-el-get.el
+++ config/init-el-get.el
@@ -9,14 +9,9 @@
 	package
         hl-sexp
 
-	(:name smex
-	       :type git
-	       :url "http://github.com/nonsequitur/smex.git")
-
-	(:name dired-plus
-	       :type git
-	       :url "git://github.com/emacsmirror/dired-plus.git")
-	
+	smex
+	dired-plus
+		
 ;;;; help typing
         auto-complete
 	highlight-parentheses
@@ -24,19 +19,14 @@
 	(:name auto-complete-octave
 	       :type emacswiki)
 	
-	(:name autopair
-	       :type git-svn
-	       :url "http://autopair.googlecode.com/svn/trunk/")
-
+	autopair
+	
 	(:name yasnippet
 	       :type git-svn
 	       :url "http://yasnippet.googlecode.com/svn/trunk/")
 
 ;;;; Cosmetics
-	(:name diminish
-	       :type http
-	       :url "http://www.eskimo.com/~seldon/diminish.el"
-	       :features diminish)
+	diminish
 
 ;;;; Org
 	(:name org-mode
@@ -50,37 +40,21 @@
 	       :features org-install)
 
 ;;;; Version control
-	(:name magit
-               :after (lambda () (global-set-key (kbd "C-x C-z") 'magit-status)))
+	magit
 
 
 ;;;; langs
 	;; html
-	;; (:name nxhtml
-	;;        :type git
-	;;        :url "http://github.com/emacsmirror/nxhtml.git")
+	;; nxhtml
 	
 	;; lisp
-        (:name paredit
-	       :type http
-	       :url "http://mumble.net/~campbell/emacs/paredit.el")
+	paredit
 
 	;; latex
-	(:name auctex
-	       :type cvs
-	       :module "auctex"
-	       :url ":pserver:anonymous@cvs.sv.gnu.org:/sources/auctex"
-	       :build ("./autogen.sh" "./configure" "make")
-	       :load  ("auctex.el" "preview/preview-latex.el")
-	       :info "doc")
+	auctex
 
 	;;yaml-mode
-	(:name yaml-mode
-	       :type git
-	       :url "git://github.com/yoshiki/yaml-mode.git"
-	       :after (lambda ()
-			(autoload 'yaml-mode "yaml-mode" nil t)
-			(add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-mode))))
+	yaml-mode
 
 ;;; sub
 	;; emacs-grads
@@ -89,62 +63,24 @@
 	       :url "git://github.com/yyr/emacs-grads.git")
 	
 ;;;; web & mail
-	(:name bbdb
-	       :type git
-	       :url "git://github.com/barak/BBDB.git"
-	       :load-path ("./lisp" "./bits")
-	       :build ("./configure" "make autoloads" "make")
-	       :build/darwin ("./configure --with-emacs=/Applications/Emacs.app/Contents/MacOS/Emacs" "make autoloads" "make")
-	       :features bbdb
-	       :info "texinfo")
-
+	bbdb
+	
 	(:name yaoddmuse
 	       :type emacswiki)
 	
-	(:name emacs-w3m
-	       :type cvs
-	       :module "emacs-w3m"
-	       :url ":pserver:anonymous@cvs.namazu.org:/storage/cvsroot"
-	       :build ("autoconf" "./configure" "make")
-	       :build/darwin ("autoconf" "./configure --with-emacs=/Applications/Emacs.app/Contents/MacOS/Emacs" "make")
-	       :info "doc"
-	       :features "w3m-load")
+	emacs-w3m
 
 	;; textile-mode
-	(:name textile-mode
-	       :type http
-	       :url "http://dev.nozav.org/scripts/textile-mode.el"
-	       :post-init (lambda ()
-			    (autoload 'textile-mode "textile-mode" "Textile editing mode." t)
-			    (add-to-list 'auto-mode-alist '("\\.textile\\'". textile-mode))))
-
+	textile-mode
 	;; markdown-mode
-	(:name markdown-mode
-	       :type git
-	       :url "git://jblevins.org/git/markdown-mode.git"
-	       :features markdown-mode
-	       :post-init (lambda ()
-			    (add-to-list 'auto-mode-alist
-					 '("\\.\\(md\\|mdown\\|markdown\\)\\'" . markdown-mode))))
-	
-
+	markdown-mode
 	;; haml-mode
-	(:name haml-mode
-	       :type git
-	       :url "https://github.com/nex3/haml-mode.git"
-	       :features haml-mode)
-	
+	haml-mode
 	;; sass mode
-	(:name sass-mode
-	       :type git
-	       :url "https://github.com/nex3/sass-mode.git"
-	       :features sass-mode)
+	sass-mode
 	
 ;;;; extras
-	(:name boxquote
-	       :type git
-	       :url "git://github.com/davep/boxquote.el.git"
-	       :after (lambda () (require 'boxquote)))
+	boxquote
 
 	(:name multi-term
 	       :type emacswiki)
@@ -152,12 +88,9 @@
 	(:name profile-dotemacs
 	       :type http
 	       :url "http://www.randomsample.de/profile-dotemacs.el")
-	
-	(:name el-get
-	       :type git
-	       :url "git://github.com/dimitri/el-get.git"
-	       :features el-get
-	       :load    "el-get.el")))
+
+	el-get
+	))
 
 
 
