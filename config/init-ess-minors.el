8dba2ba0d0204b2a5fe9e5eaddb978d3124d0e8d
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-03-17 13:33:00 +0900

    minor mode clean

--- config/init-ess-minors.el
+++ config/init-ess-minors.el
@@ -2,24 +2,18 @@
 ;; Essential minors, Change Default behavior 
 ;; -----------------------------------------------------------------------------
 
-;; enable narrowing w/out a prompt toggle -> c-x n n ; c-x n w
-(put 'narrow-to-region 'disabled nil)
-
 ;; put off menu bar scroll bar
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 
 ;; minor modes
 (tool-bar-mode -1)
-(global-font-lock-mode 1)
-(show-paren-mode 1)
 (column-number-mode 1)
-;(blink-cursor-mode -1)
+(blink-cursor-mode -1)
 ;(global-linum-mode 1)
-(set-face-attribute 'default nil :height 95)
 (setq visible-bell t)
 (show-paren-mode 1)
-(setq show-paren-style 'parenthesis)
+(display-battery-mode 1)
 (setq show-paren-delay 0)
 
 (provide 'init-ess-minors)
--- config/init-font.el
+++ config/init-font.el
@@ -8,13 +8,15 @@
  '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" 
 			 :inverse-video nil :box nil :strike-through nil 
 			 :overline nil :underline nil :slant normal :weight normal 
-			 :height 95 :width normal :foundry "unknown" 
+			 :height 90 :width normal :foundry "unknown" 
 			 :family "DejaVu Sans Mono")))))
 
 ;;(set-default-font "-adobe-courier-medium-r-normal--18-180-75-75-m-110-iso8859-1")
 
 ;;(set-default-font "-adobe-courier-medium-r-normal--18-100-75-75-m-90-iso8859-1")
 ;;(set-default-font "    -*-*-*-*-*-*-*-*-*-*-*-*-big5*-*")
+
+;;; (set-face-attribute 'default nil :height 95)
 
 
 (provide 'init-font)
