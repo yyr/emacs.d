f917b09e5c9e0488eb8ce230c861fbbf82aebc70
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-03-23 23:57:24 +0900

    git keybindings

--- config/init-git.el
+++ config/init-git.el
@@ -6,6 +6,12 @@
 (autoload 'magit-status "magit" nil t)
 
 (global-set-key (kbd "M-<f12>") 'magit-status)
+(eval-after-load 'magit-mode
+  '(progn
+     (define-key (kbd "<f7> p") 'magit-push)
+     (define-key (kbd "<f7> l") 'magit-pull)
+     ))
+
 
 (require 'rebase-mode)
 
