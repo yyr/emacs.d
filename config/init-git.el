400bcfc763eedbad6b56039036b9e7ba3bb55147
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-04-04 13:19:01 +0900

    autoloads: init-git

--- config/init-git.el
+++ config/init-git.el
@@ -1,6 +1,8 @@
-;; -----------------------------------------------------------------------------
-;; Git (magit)
-;; -----------------------------------------------------------------------------
+;;: init-git.el
+;;
+;; Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+;; Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+;; Licence: GPL v3 or later
 
 (require 'magit) 
 (autoload 'magit-status "magit" nil t)
@@ -9,10 +11,10 @@
 (eval-after-load 'magit-mode
   '(progn
      (define-key (kbd "<f7> p") 'magit-push)
-     (define-key (kbd "<f7> l") 'magit-pull)
-     ))
+     (define-key (kbd "<f7> l")
+       'magit-pull)))
 
-
-(require 'rebase-mode)
+(autoload 'rebase-mode "rebase-mode" nil t)
 
 (provide 'init-git)
+;;; init-git.el ends here
