4153c70a2c0d881289e7ad3bb1943a4087bc81a8
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-04-07 01:47:14 +0900

    battery: cite

--- config/init-battery.el
+++ config/init-battery.el
@@ -4,6 +4,8 @@
 ;; Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
 ;; Licence: GPL v3 or later
 ;;
+
+;;; http://stackoverflow.com/questions/5562974/emacs-is-it-running-on-laptop-or-on-desktop
 (require 'battery)
 (when (and battery-status-function
 	   (not (string-match-p "N/A" 
