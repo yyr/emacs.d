0e192c872ac4e6a58a82ff9d57bcbb0afeedd4e3
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-05-10 18:58:51 +0900

    new:yaml-mode.el

---
+++ config/init-yaml.el
@@ -0,0 +1,11 @@
+;;; init-yaml.el
+;;
+;; Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+;; Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+;;
+
+(autoload 'yaml-mode "yaml-mode.el" nil t)
+(add-to-list 'auto-mode-alist '("\\.\\(yml\\|yaml\\)$" . yaml-mode))
+
+
+;;; init-yaml.el ends here
