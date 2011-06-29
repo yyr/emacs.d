fa752b33a0a8826bca1fc426fdd1c5fd4ce1acef
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-06-29 16:47:39 +0900

    el-get: cleaned up for upgraded version

--- config/init-el-get.el
+++ config/init-el-get.el
@@ -6,59 +6,20 @@
 
 (setq el-get-sources
       '(
-;;;; Basics
-	;; package24
-        hl-sexp
-	byte-code-cache
-	
-	smex
-	dired-plus
-		
 ;;;; help typing
 	(:name auto-complete
 	       :type git
-	       :url "http://github.com/m2ym/auto-complete.git")
+	       :url "git://github.com/m2ym/auto-complete.git")
 	
-	highlight-parentheses
-
 	(:name yasnippet
 	       :type svn
 	       :url "http://yasnippet.googlecode.com/svn/trunk/"
 	       :features "yasnippet"
 	       :autoloads t)
 
-	(:name auto-complete-octave
+	(:name ac-octave
 	       :type emacswiki
 	       :autoloads t)
-	
-	autopair
-
-;;;; Cosmetics
-	diminish
-	color-theme
-	color-theme-zenburn
-
-;;;; Org
-	org-mode
-	;; org publish
-	htmlize
-	
-;;;; Version control
-	magit
-
-
-;;;; langs
-	;; html
-	;; nxhtml
-	
-	;; lisp
-	paredit
-
-	;; latex
-	auctex
-
-	;;yaml-mode
-	yaml-mode
 
 ;;; sub
 	;; emacs-grads
@@ -72,41 +33,89 @@
 	       :url "http://www.ncl.ucar.edu/Applications/Files/ncl.el"
 	       :autoloads t)
 	
-	gnuplot-mode
-	
-;;;; web & mail
-	bbdb
-	
 	(:name yaoddmuse
 	       :type emacswiki
 	       :autoloads t)
 	
 	
-	emacs-w3m
-
-	;; textile-mode
-	textile-mode
-	;; markdown-mode
-	markdown-mode
-	;; haml-mode
-	haml-mode
-	;; sass mode
-	sass-mode
-	
-;;;; extras
-	boxquote
+	(:name boxquote
+	       :type http
+	       :url "http://www.davep.org/emacs/boxquote.el"
+	       :autoloads t)
 
 	(:name multi-term
 	       :type emacswiki
 	       :autoloads t)
 	
-
 	(:name profile-dotemacs
 	       :type http
 	       :url "http://www.randomsample.de/profile-dotemacs.el"
 	       :autoloads t)
-	el-get))
+	))
+
+(setq my-packages
+      (append
+       '(
+	 ;;;; Basics
+	 ;; package24
+	 hl-sexp
+	 byte-code-cache
+	 
+	 highlight-parentheses
+
+	 smex
+	 dired-plus
+	 autopair
+
+	 
+
+;;;; Cosmetics
+	 diminish
+	 color-theme
+	 color-theme-zenburn
+
+;;;; Org
+	 org-mode
+	 ;; org publish
+	 htmlize
+	 
+;;;; Version control
+	 magit
 
 
-(el-get 'sync)
+;;;; langs
+	 ;; html
+	 ;; nxhtml
+	 
+	 ;; lisp
+	 paredit
+
+	 ;; latex
+	 auctex
+
+	 ;;yaml-mode
+	 yaml-mode
+	 gnuplot-mode
+	 
+;;;; web & mail
+	 bbdb
+
+	 emacs-w3m
+
+	 ;; textile-mode
+	 textile-mode
+	 ;; markdown-mode
+	 markdown-mode
+	 ;; haml-mode
+	 haml-mode
+	 ;; sass mode
+	 sass-mode
+
+	 el-get)
+
+       (loop
+	for src in el-get-sources collect (el-get-source-name src))))
+
+(el-get 'sync my-packages)
+
 (provide 'init-el-get)
