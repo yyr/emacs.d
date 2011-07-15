a133340e81a60918867fdcf84743434f2d729af7
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-07-15 17:33:13 +0900

    el-get: add epresent

--- config/init-el-get.el
+++ config/init-el-get.el
@@ -2,115 +2,120 @@
   (error "Please bootstrap el-get using the instructions here: http://github.com/dimitri/el-get/, then restart Emacs"))
 
 (setq el-get-is-lazy 't)
-(setq el-get-byte-compile-at-init 't) 	; compile at init if any source is not byte compiled 
+(setq el-get-byte-compile-at-init 't)   ; compile at init if any source is not byte compiled
 
 (setq el-get-sources
       '(
 ;;;; help typing
-	(:name auto-complete
-	       :type git
-	       :url "git://github.com/m2ym/auto-complete.git")
-	
-	(:name yasnippet
-	       :type svn
-	       :url "http://yasnippet.googlecode.com/svn/trunk/"
-	       :features "yasnippet"
-	       :autoloads t)
+        (:name auto-complete
+               :type git
+               :url "git://github.com/m2ym/auto-complete.git")
 
-	(:name ac-octave
-	       :type emacswiki
-	       :autoloads t)
+        (:name yasnippet
+               :type svn
+               :url "http://yasnippet.googlecode.com/svn/trunk/"
+               :features "yasnippet"
+               :autoloads t)
+
+        (:name ac-octave
+               :type emacswiki
+               :autoloads t)
+
+
 
 ;;; sub
-	;; emacs-grads
-	(:name emacs-grads
-	       :type git
-	       :url "git://github.com/yyr/emacs-grads.git"
-	       :autoloads t)
+        ;; emacs-grads
+        (:name emacs-grads
+               :type git
+               :url "git://github.com/yyr/emacs-grads.git"
+               :autoloads t)
 
-	(:name ncl
-	       :type http
-	       :url "http://www.ncl.ucar.edu/Applications/Files/ncl.el"
-	       :autoloads t)
-	
+        (:name ncl
+               :type http
+               :url "http://www.ncl.ucar.edu/Applications/Files/ncl.el"
+               :autoloads t)
+
+;;; Misc
+
 	(:name yaoddmuse
-	       :type emacswiki
-	       :autoloads t)
-	
-	
-	(:name boxquote
-	       :type http
-	       :url "http://www.davep.org/emacs/boxquote.el")
+               :type emacswiki
+               :autoloads t)
 
-	(:name multi-term
-	       :type emacswiki
-	       :autoloads t)
-	
-	(:name profile-dotemacs
-	       :type http
-	       :url "http://www.randomsample.de/profile-dotemacs.el"
-	       :autoloads t)
-	))
+        (:name epresent
+               :type git
+               :url "git://github.com/eschulte/epresent.git")
+
+        (:name boxquote
+               :type http
+               :url "http://www.davep.org/emacs/boxquote.el")
+
+        (:name multi-term
+               :type emacswiki
+               :autoloads t)
+
+        (:name profile-dotemacs
+               :type http
+               :url "http://www.randomsample.de/profile-dotemacs.el"
+               :autoloads t)
+        ))
 
 (setq my-packages
       (append
        '(
-	 ;;;; Basics
-	 ;; package24
-	 hl-sexp
-	 byte-code-cache
-	 
-	 highlight-parentheses
+         ;;;; Basics
+         ;; package24
+         hl-sexp
+         byte-code-cache
 
-	 smex
-	 dired-plus
-	 autopair
+         highlight-parentheses
 
-	 
+         smex
+         dired-plus
+         autopair
+
+
 
 ;;;; Cosmetics
-	 diminish
-	 color-theme
-	 color-theme-zenburn
+         diminish
+         color-theme
+         color-theme-zenburn
 
 ;;;; Org
-	 org-mode
-	 ;; org publish
-	 htmlize
-	 
+         org-mode
+
 ;;;; Version control
-	 magit
+         magit
 
 
 ;;;; langs
-	 ;; html
-	 ;; nxhtml
-	 
-	 ;; lisp
-	 paredit
+         ;; html
+         ;; nxhtml
 
-	 ;; latex
-	 auctex
+         ;; lisp
+         paredit
 
-	 ;;yaml-mode
-	 yaml-mode
-	 gnuplot-mode
-	 
+         ;; latex
+         auctex
+
+         ;;yaml-mode
+         yaml-mode
+         gnuplot-mode
+
 ;;;; web & mail
-	 bbdb
+         bbdb
 
-	 emacs-w3m
+         emacs-w3m
 
-	 ;; textile-mode
-	 textile-mode
-	 ;; markdown-mode
-	 markdown-mode
-	 ;; haml-mode
-	 haml-mode
-	 ;; sass mode
-	 sass-mode
+         ;; textile-mode
+         textile-mode
+         ;; markdown-mode
+         markdown-mode
+         ;; haml-mode
+         haml-mode
+         ;; sass mode
+         sass-mode
 
-	 el-get)
+         el-get)
        (mapcar 'el-get-source-name el-get-sources)))
 
 
