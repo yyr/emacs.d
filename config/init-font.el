426b55c52b7d5612aa49809e5612c6b3b0476285
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-07-23 03:11:14 +0900

    font: small

--- config/init-font.el
+++ config/init-font.el
@@ -4,15 +4,27 @@
 ;; Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
 ;;
 
-(custom-set-faces
- '(default ((t (:slant normal :weight light 
-			 :height 95 :width extra-condensed :foundry "unknown" 
-			 :family "DejaVu Sans Mono")))))
+(set-face-attribute 'default nil
+		    :height 110
+		    :family "DejaVu Sans Mono")
 
 
-;;(set-default-font "-adobe-courier-medium-r-normal--18-180-75-75-m-110-iso8859-1")
-;;(set-default-font "-adobe-courier-medium-r-normal--18-100-75-75-m-90-iso8859-1")
-;;; (set-face-attribute 'default nil :height 95 :family "DejaVu Sans Mono")
+;;(set-default-font
+;;"-adobe-courier-medium-r-normal--18-180-75-75-m-110-iso8859-1")
+
+;;(set-default-font
+;;"-adobe-courier-medium-r-normal--18-100-75-75-m-90-iso8859-1")
+
+;; (set-face-attribute
+;; 'default nil :height 95 :family "DejaVu Sans Mono")
+
+;; (custom-set-faces
+;;  '(default ((t (:slant normal
+;; 		       :weight light 
+;; 		       :height 95
+;; 		       :width extra-condensed
+;; 		       :foundry "unknown" 
+;; 		       :family "DejaVu Sans Mono")))))
 
 
 (provide 'init-font)
