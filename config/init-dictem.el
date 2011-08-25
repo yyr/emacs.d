ea10ba1b3dd33b3f272f770d348fb5538feccc05
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-26 02:36:37 +0900

    dictem: two fun

--- config/init-dictem.el
+++ config/init-dictem.el
@@ -30,7 +30,7 @@
 
 (global-set-key "\C-zs" 'dictem-run-search)
 (global-set-key "\C-zm" 'dictem-run-match)
-(global-set-key "\C-cd" 'dictem-run-define)
+;; (global-set-key "\C-cd" 'dictem-run-define)
 
 ;; For creating hyperlinks on database names
 ;; and found matches.
@@ -52,7 +52,6 @@
 (add-hook 'dictem-postprocess-show-info-hook
           'dictem-postprocess-definition-hyperlinks)
 
-
 (define-key dictem-mode-map [tab] 'dictem-next-link)
 (define-key dictem-mode-map [(backtab)] 'dictem-previous-link)
 
@@ -60,9 +59,7 @@
       '(("_en-en"  . ("foldoc" "gcide" "wn"))))
 
 ;;; http://paste.lisp.org/display/89086
-(global-set-key
- "\C-zd"
- '(lambda ()
+(defun dictem-run-define-at-point-with-query ()
     "Query the default dict server with the word read in within this function."
     (interactive)
     (let* ((default-word (thing-at-point 'symbol))
@@ -77,8 +74,23 @@
                                                                 str))
                                   (read-string default-prompt nil nil default-word))))
       (if (= (length dictem-query) 0) nil
-        (dictem-run 'dictem-base-search "*" dictem-query ".")))))
+        (dictem-run 'dictem-base-search "*" dictem-query "."))))
 
+(defun dictem-run-define-at-point ()
+  "dictem look up for thing at point"
+  (interactive)
+  (let* ((default-word (thing-at-point 'symbol))
+	 (dictem-query (funcall #'(lambda (str)
+				    "Remove Whitespace from beginning and end of a string."
+				    (replace-regexp-in-string "^[ \n\t]*\\(.*?\\)[ \n\t]*$"
+							      "\\1"
+							      str))
+				default-word)))
+    (if (= (length dictem-query) 0) nil
+      (dictem-run 'dictem-base-search "*" dictem-query "."))))
+
+(global-set-key "\C-cd" 'dictem-run-define-at-point)
+(global-set-key "\C-zd" 'dictem-run-define-at-point-with-query)
 
 (provide 'init-dictem)
 ;;; init-dictem.el ends here
