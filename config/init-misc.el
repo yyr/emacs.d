70199a8309870ec67544cffd24d3fd9993db5642
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-30 23:52:25 +0900

    * config/init-misc.el: zap-to-char modified fun, goto-matching-paren

--- config/init-misc.el
+++ config/init-misc.el
@@ -29,4 +29,35 @@
   (interactive)
   (load-file "~/.emacs.d/init.el"))
 
+;; zap-up-to-char
+;; --------------
+;; Most of the time when I want to zap-to-char, it's to a delimeter that
+;; I'd like to be left alone rather than zapped too.
+;;
+(defun zap-up-to-char (arg char)
+  "Like standard zap-to-char, but stops just before the given character."
+  (interactive "p\ncZap up to char: ")
+  (kill-region (point)
+               (progn
+                 (search-forward (char-to-string char) nil nil arg)
+                 (forward-char (if (>= arg 0) -1 1))
+                 (point))))
+(define-key global-map [(meta ?z)] 'zap-up-to-char)           ; Rebind M-z to our version
+
+
+;; goto-matching-paren
+;; -------------------
+;; If point is sitting on a parenthetic character, jump to its match.
+;; This matches the standard parenthesis highlighting for determining which
+;; one it is sitting on.
+;;
+(defun goto-matching-paren ()
+  "If point is sitting on a parenthetic character, jump to its match."
+  (interactive)
+  (cond ((looking-at "\\s\(") (forward-list 1))
+        ((progn
+           (backward-char 1)
+           (looking-at "\\s\)")) (forward-char 1) (backward-list 1))))
+(define-key global-map [(control ?z) ?p] 'goto-matching-paren) ; Bind to C-z p
+
 (provide 'init-misc)
