cfa3bb1407962e57e771efa7f4175ba440dfb9cf
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-30 23:43:01 +0900

    * config/init-font.el: font Zoom functions

--- config/init-font.el
+++ config/init-font.el
@@ -25,5 +25,28 @@
 ;; (set-face-attribute 'default nil :family "Anonymous Pro" :height 110)
 ;; (set-default-font "-Misc-Fixed-Medium-R-Normal--10-100-75-75-C-90-ISO8859-1")
 
+;; font-size
+;; ---------
+;; Sometimes you just feel like a larger, bolder font.  Other times you
+;; want something a little smaller to get the big picture.  These adjust
+;; the current display font size by a step in either direction.
+;;
+(defun font-increase ()
+  "Make the current display font size a step larger."
+  (interactive)
+  (set-face-attribute 'default nil
+                      :height (+ (face-attribute 'default :height) 10))
+  (message "Font size: %s" (face-attribute 'default :height)))
+(define-key global-map [(control ?z) ?=] 'font-increase)      ; Bind to C-z =
+
+(defun font-decrease ()
+  "Make the current display font size a step smaller."
+  (interactive)
+  (set-face-attribute 'default nil
+                      :height (- (face-attribute 'default :height) 10))
+  (message "Font size: %s" (face-attribute 'default :height)))
+(define-key global-map [(control ?z) ?-] 'font-decrease)      ; Bind to C-z -
+
+
 (provide 'init-font)
 ;;; init-font.el ends here
