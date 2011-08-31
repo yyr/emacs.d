fb18f5b3b5e7aef0e0dd7283ff497df9111f5ea2
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-31 13:48:28 +0900

    * config/init-autopair.el: autopair dont activate for lisp mode

--- config/init-autopair.el
+++ config/init-autopair.el
@@ -4,5 +4,8 @@
 
 (require 'autopair)
 (autopair-global-mode 1) ;; enable autopair in all buffers 
+(add-hook 'emacs-lisp-mode-hook #'(lambda ()
+                                    (setq autopair-dont-activate t)))
 
 (provide 'init-autopair)
+;;; 
