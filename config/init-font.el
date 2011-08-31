e836207ea96593d16d6db5055fe18263b56a4ffc
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-31 15:22:47 +0900

    * config/init-font.el: inendation changes

--- config/init-font.el
+++ config/init-font.el
@@ -5,9 +5,9 @@
 ;;
 
 (set-face-attribute 'default nil
-		    :height 95
-		    :width 'extra-condensed
-		    :family "DejaVu Sans Mono")
+                    :height 108
+                    :width 'extra-condensed
+                    :family "DejaVu Sans Mono")
 
 ;; (set-default-font
 ;; "-adobe-courier-medium-r-normal--18-100-75-75-m-90-iso8859-1")
@@ -16,11 +16,11 @@
 
 ;; (custom-set-faces
 ;;  '(default ((t (:slant normal
-;; 		       :weight light 
-;; 		       :height 95
-;; 		       :width extra-condensed
-;; 		       :foundry "unknown"
-;; 		       :family "DejaVu Sans Mono")))))
+;;             :weight light
+;;             :height 95
+;;             :width extra-condensed
+;;             :foundry "unknown"
+;;             :family "DejaVu Sans Mono")))))
 
 ;; (set-face-attribute 'default nil :family "Anonymous Pro" :height 110)
 ;; (set-default-font "-Misc-Fixed-Medium-R-Normal--10-100-75-75-C-90-ISO8859-1")
