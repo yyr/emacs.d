ff07e526f6fb08f55d55deaaac94b791fce7c9ae
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-09-12 20:12:08 +0900

    * init-edit-server.el: git

---
+++ init-edit-server.el
@@ -0,0 +1,16 @@
+;;; init-edit-server.el
+;;
+;; Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
+;;    File: init-edit-server.el
+;;  Author: Yagnesh Raghava Yakkala <yagnesh@NOSPAM.live.com>
+;; Created: Monday, September 12 2011
+;; Licence: GPL v3 or later.
+;;  You should get a copy from <http://www.gnu.org/licenses/gpl.html>
+
+
+;;; Description:
+
+(require 'edit-server)
+(edit-server-start)
+
+;;; init-edit-server.el ends here
