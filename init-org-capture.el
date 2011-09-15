3032cafc284bd55f6ac0f54e3a8ad8ce88903df0
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-09-15 13:24:44 +0900

    * init-org-capture.el: simplify capture templates and remove clocking

--- init-org-capture.el
+++ init-org-capture.el
@@ -9,33 +9,19 @@
 ;; 3 capture templates for TODO tasks, Notes, and org-protocol (untested)
 (setq
  org-capture-templates
- (quote
-  (
-   ;; todo --> refile/Tasks
-   ("t" "todo" entry (file+headline "~/git/org/refile.org" "Tasks") "* TODO %?
-  %U   %a
-  :CLOCK:
-  :END:" :clock-in t :clock-resume t)
-   ;; note --> refile/Notes
-   ("n" "note" entry (file+headline "~/git/org/refile.org" "Notes") "* %?                                                                            :NOTE:
-  %U  a%
-  :CLOCK:
-  :END:" :clock-in t :clock-resume t)
-   ;; log --> refile/LOG
-   ("l" "log" entry (file+headline "~/git/org/refile.org" "LOG") "* %?                                                                        :LOG:
-    %T")
-   ;; log --> refile/LOG
-   ("V" "WRF log" entry (file+headline "~/git/org/wrf-log.org" "LOG") "* %?
-    %T" :prepand t)
-   ;; log --> regcm-log/LOG
-   ("R" "RegCM log" entry (file+headline "~/git/org/regcm-log.org" "LOG") "* %?
-    %T")
-   ;; log --> wrfstff-log/LOG
-   ("W" "Webstuff log" entry (file+headline "~/git/org/webstuff-log.org" "LOG") "* %? 
-    %T" :prepand t)
-   ;; log --> refile/
-
-   )))
+ '(
+  ;; todo --> refile/Tasks
+  ("t" "todo" entry (file+headline "~/git/org/refile.org" "Tasks") "* TODO %?
+  %U   %a" )
+  ;; note --> refile/Notes
+  ("n" "note" entry (file+headline "~/git/org/refile.org" "Notes") "* %?
+  %U   %a" )
+  ;; log --> refile/LOG
+  ("l" "log" entry (file+headline "~/git/org/refile.org" "LOG") "* %?
+    %T %a")
+  ;; log --> wrfstff-log/LOG
+  ("W" "Webstuff log" entry (file+headline "~/git/org/webstuff-log.org" "LOG") "* %?
+    %T %a" :prepand t)))
 
 ;; capture Templages
 (setq org-default-notes-file "~/git/org/refile.org")
