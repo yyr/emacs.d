b34fdfbbc9916f17de2eaade9e3dfe948d4e8d36
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-09-17 16:34:30 +0900

    * init.el: cleanup

--- init.el
+++ init.el
@@ -1,41 +1,43 @@
 ;;; Emacs Init
 ;;
-;; Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+;; Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
 ;; Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
 ;; Licence: GPL v3 or later
+;;
 
 ;;; ---------------------------------------------------------------------------
-(require 'cl)                            ; must have
-;;; ---------------------------------------------------------------------------
+(setq user-full-name "Yagnesh Raghava Yakkala")
+(setq user-mail-address "yagnesh@live.com")
 
 ;;; ---------------------------------------------------------------------------
-(setq user-full-name '"Yagnesh Raghava Yakkala")
-(setq user-mail-address '"yagnesh@live.com")
+;; check emacs running on which system (to load config differently
+;; for different computers)
+;; I go by hostnames which are all fixed
 
-;;; ---------------------------------------------------------------------------
-;;; check emacs running on which system (to load config differently for different computers)
-;; I go by hostnames which are fixed
-;; for system specific
 (defvar on-laptop          ; on my laptop, mostly I can run everything
   (string-match "rag" (system-name)))
 
-(defvar on-lab-computer
+(defvar on-lab-computer                 ; main lab desktop
   (string-match "okho" (system-name)))
 
-(defvar on-lab-server
+(defvar on-lab-server        ; lab cluster (hu hu built emacs24 there)
   (string-match "amu" (system-name)))
 
-;;; ---------------------------------------------------------------------------
+;;; ---------- PATH -----------------------------------------------------------
+(defconst emacs-path "~/.emacs.d")      ; emacs path
+(setq load-path (cons (expand-file-name emacs-path) load-path))
+
+;; ----------------------------------------------------------------------------
+(require 'cl)                            ; must have
+
+;; ----------------------------------------------------------------------------
+;;; any reservations in loading individual configurations (down below)
+(setq gnus-init-file (concat emacs-path "/init-gnus.el" )) ;gnus init file
 (setq *byte-code-cache-enabled* nil)
 
-(defconst emacs-path "~/.emacs.d")             ; emacs path
-(setq gnus-init-file (concat emacs-path "/init-gnus.el" )) ;gnus init file
-
-(setq load-path (cons (expand-file-name emacs-path) load-path))
 ;; ----------------------------------------------------------------------------
 ;;; load emacs individual configuration files
 ;; ----------------------------------------------------------------------------
-
 ;;; package managers
 (require 'init-elpa)
 (require 'init-el-get)
@@ -157,7 +159,6 @@
 (require 'init-server)
 (when (not on-lab-server)
   (require 'init-edit-server))
-
 
 ;;; music
 (when on-laptop                     ; I listen music only on my laptop
