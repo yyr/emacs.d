c1a19ddea649fb571217026eeab6d6aa19c19bb0
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-10-14 15:48:18 +0900

    * init-frame.el: max-emacs-window function to maximize on startup

--- init-frame.el
+++ init-frame.el
@@ -1,3 +1,7 @@
+;; Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
+;; Author: Yagnesh Raghava Yakkala <yagnesh@NOSPAM.live.com>
+;; License: GPL v3 or later
+;;
 
 ;; full screen
 (defun fullscreen ()
@@ -6,4 +10,15 @@
                        (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
 (global-set-key [f11] 'fullscreen)
 
+;;; http://stackoverflow.com/questions/7763847/maximize-emacs-on-start-upnot-the-fullscreen/7763907#7763907
+(defun max-emacs-window (&optional f)
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                         '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                         '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+
+(max-emacs-window)
+
 (provide 'init-frame)
+;;; init-frame.el ends here
