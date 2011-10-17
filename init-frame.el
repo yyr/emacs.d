84adf9152323945e490bf6f27929e9f42b27ca70
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-10-17 18:41:47 +0900

    * init-frame.el: small

--- init-frame.el
+++ init-frame.el
@@ -18,9 +18,8 @@
   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
                          '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
 
-(when (display-graphic-p)
-  (max-emacs-x-window))
-
+(when (window-system)
+   (max-emacs-x-window))
 
 (provide 'init-frame)
 ;;; init-frame.el ends here
