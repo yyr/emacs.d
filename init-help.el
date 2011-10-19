0b68084aa927dbbf3b7034e48df14ebfe69965aa
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-10-19 18:39:14 +0900

    * init-help.el   init-info.el: shuffle a little

--- init-help.el
+++ init-help.el
@@ -18,4 +18,19 @@
 (define-key help-mode-map (kbd "n") 'next-line)
 (define-key help-mode-map (kbd "p") 'previous-line)
 
+;;; http://www.masteringemacs.org/articles/2011/08/04/full-text-searching-info-mode-apropos/
+;; Custom 'apropos' key bindings
+(global-set-key (kbd "C-h C-a") 'Apropos-Prefix)
+(define-prefix-command 'Apropos-Prefix nil "Apropos (a,d,f,i,l,v,C-v,A)")
+(define-key Apropos-Prefix (kbd "a")   'apropos)
+(define-key Apropos-Prefix (kbd "C-a") 'apropos)
+(define-key Apropos-Prefix (kbd "d")   'apropos-documentation)
+(define-key Apropos-Prefix (kbd "f")   'apropos-command)
+(define-key Apropos-Prefix (kbd "c")   'apropos-command)
+(define-key Apropos-Prefix (kbd "i")   'info-apropos)
+(define-key Apropos-Prefix (kbd "l")   'apropos-library)
+(define-key Apropos-Prefix (kbd "v")   'apropos-variable)
+(define-key Apropos-Prefix (kbd "C-v") 'apropos-value)
+(define-key Apropos-Prefix (kbd "A")   'about-emacs)
+
 ;;; init-help.el ends here
--- init-info.el
+++ init-info.el
@@ -11,18 +11,4 @@
           (lambda ()
             (setq Info-additional-directory-list Info-default-directory-list)))
 
-;;; http://www.masteringemacs.org/articles/2011/08/04/full-text-searching-info-mode-apropos/
-;; Custom 'apropos' key bindings
-(global-set-key (kbd "C-h C-a") 'Apropos-Prefix)
-(define-prefix-command 'Apropos-Prefix nil "Apropos (a,d,f,i,l,v,C-v)")
-(define-key Apropos-Prefix (kbd "a")   'apropos)
-(define-key Apropos-Prefix (kbd "C-a") 'apropos)
-(define-key Apropos-Prefix (kbd "d")   'apropos-documentation)
-(define-key Apropos-Prefix (kbd "f")   'apropos-command)
-(define-key Apropos-Prefix (kbd "c")   'apropos-command)
-(define-key Apropos-Prefix (kbd "i")   'info-apropos)
-(define-key Apropos-Prefix (kbd "l")   'apropos-library)
-(define-key Apropos-Prefix (kbd "v")   'apropos-variable)
-(define-key Apropos-Prefix (kbd "C-v") 'apropos-value)
-
 ;;; init-info.el ends here
