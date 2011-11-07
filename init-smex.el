2f4d67e4443e105dbd7b127b6a63b0c695ef8129
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-11-07 22:17:37 +0900

    * init-yasnip.el: keep only snippets written by me. not whole   downloaded snippets are removed from the repository

--- init-smex.el
+++ init-smex.el
@@ -1,6 +1,16 @@
+;;; init-smex.el
+;;
+;; Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
+;; Author: Yagnesh Raghava Yakkala <yagnesh@NOSPAM.live.com>
+;; License: GPL v3 or later
+
 ;; -----------------------------------------------------------------------------
 ;; ido completion in M-x
 ;; -----------------------------------------------------------------------------
 (require 'smex)
+
 (smex-initialize)
-(global-set-key "\M-x" 'smex)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;;; init-smex.el ends here
--- init-yasnip.el
+++ init-yasnip.el
@@ -1,13 +1,17 @@
-;; -----------------------------------------------------------------------------
-;; yasnippet
-;; -----------------------------------------------------------------------------
+;; init-yasnip.el
 
-(require 'yasnippet) ;; yasnippet-bundle
+(require 'yasnippet)
+
 (setq yas/snippet-dirs '"~/.emacs.d/snippets/")
 (yas/initialize)
 
-(setq yas/root-directory '( "~/.emacs.d/snippets/"))
-                                        ; Map `yas/load-dir/")ectory' to every element
+(setq yas/root-directory
+      '( "~/.emacs.d/snippets/"
+         "~/.emacs.d/el-get/yasnippet/snippets/"))
+
 (mapc 'yas/load-directory yas/root-directory)
 
-(add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
+(add-to-list 'auto-mode-alist
+             '("\\.yasnippet$" . snippet-mode))
+
+;;; init-yasnip.el ends here
