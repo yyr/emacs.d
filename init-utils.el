e2328a9c4abdddfeb99f07c3ba4253dbae85da71
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-11-25 12:31:23 +0900

    * init-utils.el: shell-command-on-buffer-file

--- init-utils.el
+++ init-utils.el
@@ -167,4 +167,15 @@ License: GPL v3 or later
         (cons (car (car asslist)) (ca-all-asscs (cdr asslist) query))
       (ca-all-asscs (cdr asslist) query)))))
 
+;;; ----
+(defun shell-command-on-buffer-file ()
+  "prompts for a command and executes that command on to the associated
+ file of current buffer. if no buffer is associated gives an error"
+  (interactive)
+  (or (buffer-file-name) (error "no file is associated file to this buffer"))
+  (let* ((my-cmd (read-shell-command "Command to run: "))
+         (cmd-to-run (concat my-cmd " " (buffer-file-name))))
+    (shell-command cmd-to-run)))
+
+
 ;;; init-utils-el ends here
