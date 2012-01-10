57207827bf66f1ae3fd8ceea1c01b1fe08259107
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-01-11 00:23:08 +0900

    * init-dired.el: good

--- init-dired.el
+++ init-dired.el
@@ -22,3 +22,22 @@
 (setq dired-bind-jump t)
 (global-set-key "\C-x\C-j" 'dired-jump)
 (global-set-key "\C-x4\C-j" 'dired-jump-other-window)
+
+;;; from org mailing list Eric Abrahamsen
+(defun dired-xdg-open-file ()
+  "Opens the current file in a Dired buffer."
+  (interactive)
+  (xdg-open-file (dired-get-file-for-visit)))
+
+(defun xdg-open-file (filename)
+  "xdg-opens the specified file."
+  (interactive "fFile to open: ")
+  (let ((process-connection-type nil))
+    (start-process "" nil "/usr/bin/xdg-open" filename)))
+
+;;'e' usually does 'dired-find-file, same as RET, rebinding it here
+(add-hook 'dired-mode-hook
+          (lambda ()
+            (define-key dired-mode-map (kbd "e") 'dired-xdg-open-file)))
+
+(setq image-dired-external-viewer "/usr/bin/xdg-open")
