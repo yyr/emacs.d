ce4f3f4a048fd08cfbe12dfda716bfd7092d47d5
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-03-12 21:12:49 +0900

    * init-navigation.el: kbd shortcuts navigation

--- init-navigation.el
+++ init-navigation.el
@@ -10,4 +10,9 @@
 (define-key global-map
   (kbd "M-l") 'ace-jump-mode)
 
+
+;;; faster C-n C-p
+(global-set-key (kbd "C-S-n") (kbd "C-6 C-n"))
+(global-set-key (kbd "C-S-p") (kbd "C-6 C-p"))
+
 ;;; init-navigation.el ends here
