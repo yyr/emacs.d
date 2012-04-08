d70e3383cc61a5f814db6fa541fd20a2e4b59b9d
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-04-09 04:49:34 +0900

    * init-mail.el: working msmtp setup

--- init-mail.el
+++ init-mail.el
@@ -9,17 +9,37 @@
 ;; mail settings
 ;; apply even before gnus starts.
 
-;; Smtp
-;;;------------------------------------------------
-(setq send-mail-function 'smtpmail-send-it)
+;;; msmtp
+;;---------------------
 (require 'smtpmail)
 
-(setq message-send-mail-function 'smtpmail-send-it)
-(setq smtpmail-smtp-server "localhost")  ; use local server postfix
-(setq smtpmail-smtp-service "smtp")     ; through localhost server
+(setq message-send-mail-function 'message-send-mail-with-sendmail)
+;; we substitute sendmail with msmtp
+(setq sendmail-program "/usr/bin/msmtp")
 
-;(setq smtpmail-debug-info t) ; only to debug problems
-(setq smtpmail-stream-type 'starttls)
+(defun cg-feed-msmtp ()
+  "Switch msmtp args by looking at the `From' address in the mail headers,
+"
+  (if (message-mail-p)
+      (save-excursion
+        (let* ((from
+                (save-restriction
+                  (message-narrow-to-headers)
+                  (message-fetch-field "From")))
+               (account
+                (cond
+                 ;; CHANGE HERE according to your mail .msmtprc
+                 ((string-match "live.com" from)
+                  "live")
+                 ;; CHANGE HERE according to your mail id
+                 ((string-match "yagneshmsc" from)
+                  "gmail"))))
+          (message "mstmp args given: -a %s" account)
+          (setq message-sendmail-extra-arguments (list '"-a" account))))))
+
+(add-hook 'message-send-mail-hook 'cg-feed-msmtp)
+(setq message-sendmail-envelope-from 'header)
+
 
 (add-to-list 'auto-mode-alist '("\\.eml$" . message-mode))
 ;;; init-mail.el ends here
