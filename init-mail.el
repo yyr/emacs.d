b366d947d3d2f3406d4f6ebbf7006bb7b3d64a07
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-04-10 16:02:45 +0900

    * init-mail.el: one more rule to msmtp

--- init-mail.el
+++ init-mail.el
@@ -30,21 +30,22 @@ NOTE: for a \"gmail\" switch there should be corresponding
                   (message-fetch-field "From")))
                (account
                 (cond
-                 ;; CHANGE HERE according to your mail .msmtprc
+                 ;; gmail
                  ((string-match "live.com" from)
                   "live")
-                 ;; CHANGE HERE according to your mail id
+                 ;; live
                  ((string-match "yagneshmsc" from)
                   "gmail")
                  ;; lab mail
                  ((string-match "hokudai" from)
-                  "hokudai"))))
+                  "hokudai")
+                 (t
+                  "default"))))
           (message "mstmp args given: -a %s" account)
           (setq message-sendmail-extra-arguments (list '"-a" account))))))
 
 (add-hook 'message-send-mail-hook 'msmtp-switch-args)
 (setq message-sendmail-envelope-from 'header)
-
 
 (add-to-list 'auto-mode-alist '("\\.eml$" . message-mode))
 ;;; init-mail.el ends here
