036636f68dc722dcb2a2af60ce7f1fc162733855
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-04-11 18:53:59 +0900

    * init-ncl.el: cleanup

--- init-ncl.el
+++ init-ncl.el
@@ -1,37 +1,33 @@
 ;;; init-ncl.el
 ;;
-;; Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
-;;
 
-;;; http://www.ncl.ucar.edu/Applications/editor.shtml
-;; this line associates ncl-mode with the lisp package that defines it.
-
-;;; load ncl
+;; load ncl using ncl.
 (el-get 'sync '(ncl))
 
+;;; Common
 (autoload 'ncl-mode "ncl" "ncl-mode for editing ncar graphics" t)
-(add-to-list 'ac-dictionary-directories
-             "~/.emacs.d/el-get/ncl/dict")
-
-;;; append to the last
-(setq yas/snippet-dirs
-      (append yas/snippet-dirs '("~/.emacs.d/el-get/ncl/snippets/")))
-
 (setq auto-mode-alist (cons '("\.ncl$" . ncl-mode) auto-mode-alist))
 (setq ncl-startup-message nil)
 
-;;; (add-hook 'ncl-mode-hook 'ncl-doc)
-
-;;; imenu support for ncl-mode
+;;; load ncl-doc
 (add-hook 'ncl-mode-hook
           (lambda ()
             (require 'ncl-doc)
             (ncl-doc-minor-mode 1)))
 
-(setq ncl-doc-url-base "http://www.ncl.ucar.edu")
-;(setq ncl-doc-url-base (directory-file-name "file:///home/yagnesh/docs/ncl"))
+;; use downloaded documentation instead (for offline use)
+;; (setq ncl-doc-url-base (directory-file-name "file:///home/yagnesh/docs/ncl"))
+
+;; auto-completion setup
+(add-to-list 'ac-dictionary-directories
+             "~/.emacs.d/el-get/ncl/dict")
+
+;; yasnippet setup
+(setq yas/snippet-dirs
+      (append yas/snippet-dirs '("~/.emacs.d/el-get/ncl/snippets/")))
 
 
+;; utility functions
 (defun ncl-del-comment-indentation ()
   "remove line beginning comment indentation"
   (interactive "r")
