0b98d623d2b3bce934f5a6d6484874ac9cd1bd6c
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-05-19 20:31:49 +0900

    * init-general.el: unset c-x c-e

--- init-general.el
+++ init-general.el
@@ -120,4 +120,7 @@ before point if point is past text"
 (add-hook 'doc-view-mode-hook (lambda ()
                            (auto-revert-mode 1)))
 
+;;; stop unwanted kill-emacs (while using orgmode)
+(global-unset-key (kbd "C-x C-c"))
+
 ;;; init-general.el ends here
