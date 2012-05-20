fbfdf0291f49bdb40182200bebb1108714dba70c
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-05-20 22:37:45 +0900

    * minimal.el: better

--- minimal.el
+++ minimal.el
@@ -6,26 +6,33 @@
 ;;; call emacs like this
 ;; emacs -Q -l ~/.emacs.d/minimal.el
 
+;;; bug report
 (setq user-full-name "Yagnesh Raghava Yakkala")
+(setq user-mail-address "hi@yagnesh.org")
 
 ;;; some convenience
 (defalias 'yes-or-no-p 'y-or-n-p)
 (when (boundp 'ido-mode)
   (ido-mode))
 
-;;; debugging package
+;;; package to debug
 (setq *debug-org* nil)
 
-
 ;;; org
+(setq org-install-dir "~/.emacs.d/el-get/org-mode")
+
+
 (when *debug-org*
   ;; load org first
-  (add-to-list 'load-path "~/.emacs.d/el-get/org-mode/lisp")
+  (add-to-list 'load-path (concat org-install-dir "/contrib/lisp"))
+  (add-to-list 'load-path (concat org-install-dir "/lisp"))
+  (message "icame here" )
   (require 'org-install)
+  (require 'org-export)
 
-  ;; do stuff here
+  ;; reproduce bug here
+
 
   )
-
 
 ;;; minimal.el ends here
