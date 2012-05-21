02d98e3f3f4bb63c9041e80d97f7891523d03f77
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-05-21 20:46:31 +0900

    * minimal.el: clean up

--- minimal.el
+++ minimal.el
@@ -10,29 +10,57 @@
 (setq user-full-name "Yagnesh Raghava Yakkala")
 (setq user-mail-address "hi@yagnesh.org")
 
+(setq debug-on-error t)
+
+
 ;;; some convenience
 (defalias 'yes-or-no-p 'y-or-n-p)
 (when (boundp 'ido-mode)
   (ido-mode))
 
+;;; package dirs
+(setq el-get-install-dir "~/.emacs.d/el-get")
+
 ;;; package to debug
 (setq *debug-org* nil)
+(setq *debug-o-blog* nil)
 
-;;; org
+
+;;;------------------------------------------------------------------
+;;;  org
 (setq org-install-dir "~/.emacs.d/el-get/org-mode")
 
+(defun minimal-load-org ()
+  (add-to-list 'load-path (concat org-install-dir "/contrib/lisp"))
+  (add-to-list 'load-path (concat org-install-dir "/lisp"))
+  (require 'org-install))
 
 (when *debug-org*
   ;; load org first
-  (add-to-list 'load-path (concat org-install-dir "/contrib/lisp"))
-  (add-to-list 'load-path (concat org-install-dir "/lisp"))
-  (message "icame here" )
-  (require 'org-install)
-  (require 'org-export)
+  (minimal-load-org)
 
   ;; reproduce bug here
 
+  )
+
+
+;;;------------------------------------------------------------------
+;;;  o-blog
+(defun minimal-load-o-blog ()
+  (minimal-load-org)
+  (add-to-list 'load-path (concat el-get-install-dir "/o-blog"))
+  (require 'o-blog)
+  (require 'o-blog-bootstrap)
+  )
+
+
+(when *debug-o-blog*
+  ;; load first
+  (minimal-load-o-blog)
 
   )
+
+
+
 
 ;;; minimal.el ends here
