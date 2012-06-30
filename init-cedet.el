b8a2cbb4f9fb8bf4546db580e46b2762249b9f07
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-06-30 18:53:55 +0900

    * init-cedet.el: cleanup

--- init-cedet.el
+++ init-cedet.el
@@ -12,10 +12,7 @@
   (when (file-directory-p "~/.emacs.d/el-get/cedet")
     (progn
       (add-to-list 'load-path  "~/.emacs.d/el-get/cedet")
-      (load-file "~/.emacs.d/el-get/cedet/cedet-devel-load.el")
-      (semantic-load-enable-minimum-features)
-      (semantic-load-enable-code-helpers))))
-
+      (load-file "~/.emacs.d/el-get/cedet/cedet-devel-load.el"))))
 
 ;;; ede
 (setq semantic-default-submodes
@@ -26,41 +23,36 @@
         global-semantic-idle-local-symbol-highlight-mode
         ;; global-cedet-m3-minor-mode
 
-        global-semantic-stickyfunc-mode))
+        ;; code helpers
+        ;; global-semantic-idle-scheduler-mode
+        ;; global-semantic-idle-summary-mode
+        ;; global-semantic-idle-completions-mode
+
+        ;; eye candy
+        ;; global-semantic-decoration-mode
+        ;; global-semantic-highlight-func-mode
+        ;; global-semantic-highlight-edits-mode
+        ;; global-semantic-stickyfunc-mode
+
+        ;; debugging semantic itself
+        ;;global-semantic-show-parser-state-mode 1   ;; show the parsing state in the mode line
+        ;;global-semantic-show-unmatched-syntax-mode 1
+        ))
 
 (global-ede-mode 1)
-
-(semantic-add-system-include
- "/home/yagnesh/local/python/lib/python3.2/" 'python-mode)
 
 (defun my-cedet-hook ()
   (local-set-key [(control return)] 'semantic-ia-complete-symbol)
   (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
   (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
-  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle))
 
-  ;; code helpers
-  (global-semantic-idle-scheduler-mode)
-  (global-semantic-idle-summary-mode)
-  (global-semantic-idle-completions-mode)
-
-
-  ;; eye candy
-  (global-semantic-decoration-mode)
-  (global-semantic-highlight-func-mode)
-  (global-semantic-highlight-edits-mode)
-
-  ;; debugging semantic itself
-  ;;(global-semantic-show-parser-state-mode 1)   ;; show the parsing state in the mode line
-  ;;(global-semantic-show-unmatched-syntax-mode 1)
-  )
-
-
-  (dolist (hook '(python-mode-hook
-                c-mode-hook
-                fortran-mode-hook
-                f90-mode-hook))
+  (dolist (hook
+           '(python-mode-hook
+             c-mode-hook
+             fortran-mode-hook
+             emacs-lisp-mode-hook
+             f90-mode-hook))
   (add-hook hook 'my-cedet-hook))
-
 
 ;;; init-cedet.el ends here
