7eef4f4fd695e0d1edf457e6ac99351905451d77
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-06-30 19:23:19 +0900

    * init-exec-path.el: add func

--- init-exec-path.el
+++ init-exec-path.el
@@ -2,3 +2,9 @@
   (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
+
+(defun add-to-load-path (PATH)
+  "Add PATH to your load-path."
+  (interactive (list (read-directory-name "Directory: " default-directory
+                                          nil  t default-directory)))
+  (add-to-list 'load-path path))
