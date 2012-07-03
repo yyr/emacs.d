d01abe33d5a946c7451c1cd29bc6bec49274bf61
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-07-03 10:56:10 +0900

    * init-smex.el: require smex

--- init-smex.el
+++ init-smex.el
@@ -8,6 +8,7 @@
 ;; -----------------------------------------------------------------------------
 (el-get 'sync '(smex))
 
+(require 'smex)
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
