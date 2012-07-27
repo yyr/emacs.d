7ff5482b5e6178bec42add27ab983e5033d5a523
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-07-27 14:41:50 +0900

    * init-edict.el: edict (TODO put it all in dictonary)

---
+++ init-edict.el
@@ -0,0 +1,43 @@
+;;; init-edict.el
+;;
+;; Author: Yagnesh Raghava Yakkala. http://yagnesh.org
+;;    File: init-edict.el
+;; Created: Friday, July 27 2012
+
+;;; Description:
+;;
+
+(el-get 'sync '(edict))
+
+(setq edict-dictionaries
+      (list
+       "/usr/share/edict/edict"
+       "/usr/share/edict/kanjidic"
+       "/usr/share/edict/lingdic"))
+
+(add-to-list 'auto-coding-alist '("edict\\'" . euc-jp))
+(add-to-list 'auto-coding-alist '("kanjidic\\'" . euc-jp))
+
+;; (setq edict-dictionaries
+;;       (list
+;;        "/usr/share/edict/4jwords"
+;;        "/usr/share/edict/aviation"
+;;        "/usr/share/edict/classical"
+;;        "/usr/share/edict/compdic"
+;;        "/usr/share/edict/compverb"
+;;        "/usr/share/edict/concrete"
+;;        "/usr/share/edict/edict"
+;;        "/usr/share/edict/edicth"
+;;        "/usr/share/edict/enamdict"
+;;        "/usr/share/edict/findic"
+;;        "/usr/share/edict/geodic"
+;;        "/usr/share/edict/j_places"
+;;        "/usr/share/edict/jddict.v02"
+;;        (cons "/usr/share/edict/kanjd212"  'euc-jp)
+;;        "/usr/share/edict/kanjidic"
+;;        "/usr/share/edict/lingdic"
+;;        "/usr/share/edict/mktdic"
+;;        "/usr/share/edict/pandpdic"))
+
+
+;;; init-edict.el ends here
