5ce4eded5f4b263588d78d193f83196a40a0318e
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-08-30 18:04:54 +0900

    * init-auto-complete.el: cleanup

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -15,50 +15,54 @@
 (add-to-list 'ac-sources 'ac-source-semantic)
 
 (setq ac-auto-start 2)
-
-(setq ac-dwim t)
 (setq ac-delay 0.)
-(define-key ac-completing-map (kbd "C-n") 'ac-next)
-(define-key ac-completing-map (kbd "C-p") 'ac-previous)
-
-(dolist (mode
-         '(emacs-lisp-mode   lisp-interaction-mode
-           magit-log-edit-mode log-edit-mode org-mode text-mode
-           haml-mode sass-mode yaml-mode csv-mode espresso-mode
-           haskell-mode html-mode nxml-mode sh-mode smarty-mode
-           clojure-mode lisp-mode textile-mode markdown-mode
-           tuareg-mode cperl-mode sass-mode ncl-mode latex-mode
-           fortran-mode f90-mode))
-  (add-to-list 'ac-modes mode))
-
-(add-hook 'auto-complete-mode-hook (lambda ()
-                                     (ac-flyspell-workaround)))
-
-(define-key ac-completing-map   "\t"    'ac-expand-common)
-(define-key ac-completing-map (kbd "RET") 'ac-complete)
-;; (define-key ac-completing-map   "\r"    'ac-complete)
-;; (define-key ac-completing-map   "\M-/"  'ac-stop)
-
-;; Use Emacs' built-in TAB completion hooks to trigger AC (Emacs >= 23.2)
-(setq tab-always-indent
-      'complete) ;; use 'complete when auto-complete is disabled
+(setq ac-dwim t)
 (add-to-list 'completion-styles 'initials t)
 
+(define-key ac-completing-map (kbd "C-n") 'ac-next)
+(define-key ac-completing-map (kbd "C-p") 'ac-previous)
+;; (define-key ac-completing-map   "\M-/"  'ac-stop)
+(define-key ac-completing-map   "\t"    'ac-expand-common)
+(define-key ac-completing-map (kbd "RET") 'ac-complete)
 
-;;; http://www.masteringemacs.org/articles/2010/11/29/evaluating-elisp-emacs/
-;; define  sources for ielm
-(defun ielm-auto-complete ()
-  "Enables `auto-complete' support in \\[ielm]."
-  (setq ac-sources '(ac-source-functions
-                     ac-source-variables
-                     ac-source-features
-                     ac-source-symbols
-                     ac-source-words-in-same-mode-buffers))
-  (add-to-list 'ac-modes 'inferior-emacs-lisp-mode)
-  (auto-complete-mode 1))
-(add-hook 'ielm-mode-hook 'ielm-auto-complete)
 
-;;; work around for autopair autocomplete
+;;; work around for autopair auto-complete
 (define-key ac-completing-map [return] 'ac-complete)
+(add-hook 'auto-complete-mode-hook (lambda ()
+                                     (ac-flyspell-workaround)))
+;; Use Emacs' built-in TAB completion hooks to trigger AC (Emacs >= 23.2)
+(setq tab-always-indent 'complete) ;; use 'complete when auto-complete is disabled
+
+
+;;; list of modes where ac should be available
+(dolist (mode '(emacs-lisp-mode
+                lisp-interaction-mode
+                inferior-emacs-lisp-mode
+                magit-log-edit-mode
+                log-edit-mode
+                org-mode
+                text-mode
+                haml-mode
+                sass-mode
+                yaml-mode
+                haskell-mode
+                html-mode
+                nxml-mode
+                sh-mode
+                lisp-mode
+                textile-mode
+                markdown-mode
+                cperl-mode
+                sass-mode
+                latex-mode
+                fortran-mode
+                f90-mode))
+
+  (add-to-list 'ac-modes mode))
+
+;;; configuration of ac-sources
+;; define  sources for ielm
+(add-hook 'ielm-mode-hook (lambda ()
+                            (setq-default ac-sources 'ac-emacs-lisp-mode-setup)))
 
 ;;; init-auto-complete.el ends here
