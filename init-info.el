17e3611e0342a11d10e3b89ab53614cbeb9418df
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-12-28 08:39:56 +0900

    * init-info.el: Fix info path

--- init-info.el
+++ init-info.el
@@ -8,4 +8,10 @@
 (require 'info-look)
 (add-to-list 'Info-additional-directory-list "~/.emacs.d/my-info/")
 
+;;; this work around is needed if emacs is in non standard directory and
+;;; INFOPATH is set.
+(if (getenv "INFOPATH")
+    (dolist (dir Info-default-directory-list)
+      (add-to-list 'Info-additional-directory-list dir)))
+
 ;;; init-info.el ends here
