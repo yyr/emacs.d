fbb89e5f951ff3e7279e905986eaa9e6757b603b
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-01-09 14:05:06 +0900

    * init-org.el: remove old commented code.
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- init-org.el
+++ init-org.el
@@ -42,18 +42,6 @@
 (global-set-key (kbd "<f12>") 'org-agenda)
 (define-key org-mode-map (kbd "C-c C-e") 'org-export-dispatch )
 
-;;; Eric Schulte org list
-;; (defun yas/org-very-safe-expand ()
-;;   (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
-
-;; (remove-hook 'org-mode-hook
-;;           (lambda ()
-;;             ;; yasnippet (using the new org-cycle hooks)
-;;             (make-variable-buffer-local 'yas/trigger-key)
-;;             (setq yas/trigger-key [tab])
-;;             (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
-;;             (define-key yas/keymap [tab] 'yas/next-field)))
-
 ;;; save org-buffers every hour
 (run-at-time "00:59" 3600 'org-save-all-org-buffers)
 
