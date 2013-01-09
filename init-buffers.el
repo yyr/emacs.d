debfbe10bc88f33077ec69ae7f94d2179a8d3b50
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-01-09 14:15:43 +0900

    * init-buffers.el: show unicode art, cleanup redundant comments.
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- init-buffers.el
+++ init-buffers.el
@@ -1,19 +1,19 @@
 ;;; init-buffers.el
-;;
-;; Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
-;;    File: init-buffers.el
 ;; Created: Tuesday, August 30 2011
-;; License: GPL v3 or later. You should get a copy from <http://www.gnu.org/licenses/gpl.html>
-
-;;; Description:
-;;
 
 (require 'uniquify)       ; Smarter buffer naming than x<1>, x<2>, ...
+
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets
       uniquify-after-kill-buffer-p t
       uniquify-ignore-buffers-re "^\\*")
 
 ;;; Ibuffer
+;;; --------------------------------------------------------
+(setq ibuffer-modified-char ?✍)
+(setq ibuffer-read-only-char ?✗)
+(setq ibuffer-marked-char ?✓)
+(setq ibuffer-deletion-char ?␡)
+
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (setq ibuffer-expert t)
 (setq ibuffer-show-empty-filter-groups nil)
@@ -84,21 +84,13 @@
                                                  activate)
   (setq ad-return-value (nreverse ad-return-value)))
 
-;; kill-other-buffers
-;; ------------------
-;; Emacs buffers multiply faster than rabbits.  They were
-;; regenerating faster than I could kill them so I wrote this.  Run this
-;; macro to kill all but the active buffer and the unsplit the window if
-;; need be.
-;;
-
 (defun kill-other-buffers ()
   "Kill all buffers except the current and unsplit the window."
   (interactive)
-  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))) ; Delete other buffers
-  (delete-other-windows)           ; And then unsplit the current window...
-  (delete-other-frames))           ; ...and remove other frames, too.
-(define-key global-map [(control ?z) ?k] 'kill-other-buffers) ; Bind to C-z k
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list)))
+  (delete-other-windows)
+  (delete-other-frames))
+(define-key global-map [(control ?z) ?k] 'kill-other-buffers)
 
 
 (defun kill-star-buffers ()
@@ -110,12 +102,11 @@ FIXME: make an exceptions for few buffers like gnus, emms"
               (kill-buffer buf)))
         (buffer-list)))
 (define-key global-map
-  [(control ?z) (control ?k)] 'kill-star-buffers) ; Bind to C-z C-k
+  [(control ?z) (control ?k)] 'kill-star-buffers)
 
 ;;; kill current buffer
 (global-set-key (kbd "M-k") (lambda ()
                               (interactive)
                               (kill-buffer (buffer-name))))
-
 
 ;;; init-buffers.el ends here
