739e3b1083cbb71221f46af9dcb32f317a2641b1
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-01-23 15:52:39 +0900

    * insert/* clean
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- insert/insert.pl
+++ insert/insert.pl
@@ -1,16 +1,5 @@
 #!/usr/bin/env perl
-#
-# Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
-#    File: (>>FILE<<)
 # Created: (>>DATE<<)
-# License: GPL v3 or later. <http://www.gnu.org/licenses/gpl.html>
-#
-
-# Description:
-#
 
 use strict;
 use warnings;
-
-
-# (>>FILE<<) ends here
--- insert/insert.sh
+++ insert/insert.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-#
-# Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
-#    File: (>>FILE<<)
 # Created: (>>DATE<<)
-# License: GPL v3 or later.  <http://www.gnu.org/licenses/gpl.html>
-#
-
-# Description:
-
-
-# (>>FILE<<) ends here
