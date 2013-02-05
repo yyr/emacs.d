c296feec706965fff302317089e606e71c99afcf
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-02-06 01:53:43 +0900

    * init-info.el: add info, pydoc-info
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- init-info.el
+++ init-info.el
@@ -6,12 +6,22 @@
 ;; info , searching docs, short cuts
 
 (require 'info-look)
-(add-to-list 'Info-additional-directory-list "~/.emacs.d/my-info/")
+(setq my-info-dir "~/git/info-collection/")
+(add-to-list 'Info-additional-directory-list my-info-dir)
 
 ;;; this work around is needed if emacs is in non standard directory and
 ;;; INFOPATH is set.
 (if (getenv "INFOPATH")
     (dolist (dir Info-default-directory-list)
       (add-to-list 'Info-additional-directory-list dir)))
+
+(when (file-exists-p (concat my-info-dir "python.info"))
+  (el-get 'sync 'pydoc-info)
+  (info-lookup-add-help
+   :mode 'python-mode
+   :parse-rule 'pydoc-info-python-symbol-at-point
+   :doc-spec
+   '(("(python)Index" pydoc-info-lookup-transform-entry)
+     ("(sphinx)Index" pydoc-info-lookup-transform-entry))))
 
 ;;; init-info.el ends here
