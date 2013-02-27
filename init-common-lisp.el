724085c766a93057ff8827bbd83d37f66627adb8
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-02-27 16:59:05 +0900

    * init-common-lisp.el: clean off slime thingy
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- init-common-lisp.el
+++ init-common-lisp.el
@@ -7,33 +7,6 @@
 ;; See http://bc.tech.coop/blog/070927.html
 (add-to-list 'auto-mode-alist '("\\.cl$" . lisp-mode))
 
-(setq load-path (cons
-                 (expand-file-name
-                  "~/.emacs.d/el-get/slime/contrib/")
-                 load-path))
-
-(setq inferior-lisp-program "/usr/bin/sbcl") ; your Lisp system
-(add-hook 'lisp-mode-hook (lambda ()
-                            (cond ((not (featurep 'slime))
-                                   (require 'slime)
-                                   (normal-mode)))))
-
-
-(eval-after-load "slime"
-  '(progn
-     (add-to-list 'slime-lisp-implementations
-                  '(sbcl ("sbcl") :coding-system utf-8-unix))
-     (add-to-list 'slime-lisp-implementations
-                  '(cmucl ("cmucl") :coding-system iso-latin-1-unix))
-     (require 'slime-fancy)
-     (require 'slime-banner)
-     (require 'slime-asdf)
-     (slime-banner-init)
-     (slime-asdf-init)
-     (setq slime-complete-symbol*-fancy t)
-     (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
-     (slime-setup)))
-
 ;; From http://bc.tech.coop/blog/070515.html
 (defun lispdoc ()
   "Searches lispdoc.com for SYMBOL, which is by default the symbol currently under the curser"
