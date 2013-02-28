cca01f7a9d3840e65287252e5e1bae503a3f1894
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-02-28 11:36:01 +0900

    * init-git.el: W for toggling white space diff hunks
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- init-git.el
+++ init-git.el
@@ -12,7 +12,29 @@
 (setq magit-save-some-buffers nil)
 (setq magit-remote-ref-format 'remote-slash-branch)
 
+(setq magit-diff-refine-hunk t)
+
 ;;; rebase mode
 (autoload 'rebase-mode "rebase-mode" nil t)
+
+;;; http://whattheemacsd.com/setup-magit.el-02.html
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(eval-after-load "magit"
+  '(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace))
 
 ;;; init-git.el ends here
