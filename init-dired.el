30ef0d043bedd536eee732340e08e7c29241b78a
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-03-01 14:37:57 +0900

    * init-dired.el: clean
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- init-dired.el
+++ init-dired.el
@@ -4,30 +4,35 @@
 
 ;;;; navigation
 
-;; (el-get 'sync '(dired-plus))
-;; (require 'dired+)
-;; (define-key ctl-x-map   "d" 'diredp-dired-files)
-;; (define-key ctl-x-4-map "d" 'diredp-dired-files-other-window)
+(el-get 'sync 'dired-details) ;; Make dired less verbose
 
 (setq dired-recursive-deletes 'always)
 (setq dired-listing-switches "-alh")
-(add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
+
+(add-hook 'dired-mode-hook
+          (lambda ()
+            ('turn-on-gnus-dired-mode)
+            ;; dired-x
+            (require 'dired-x)
+            (require 'dired-details)
+            (require 'dired-aux)
+            (setq-default dired-details-hidden-string "--- ")
+            (dired-details-install)
+            (dired-omit-mode 1)))
 
 (define-key dired-mode-map [mouse-2] 'dired-find-file)
-
-;; dired-x
-(add-hook 'dired-load-hook
-          (function (lambda () (load "dired-x"))))
 
 (autoload 'dired-jump "dired-x"
   "Jump to dired buffer corresponding to current buffer."
   'interactive)
+
 (autoload 'dired-jump-other-window "dired-x"
   "Like \\[dired-jump] (`dired-jump') but in other window."
   'interactive)
 (setq dired-bind-jump t)
 (global-set-key "\C-x\C-j" 'dired-jump)
 (global-set-key "\C-x4\C-j" 'dired-jump-other-window)
+
 
 ;;; from org mailing list Eric Abrahamsen
 (defun dired-xdg-open-file ()
@@ -44,8 +49,8 @@
 ;;'e' usually does 'dired-find-file, same as RET, rebinding it here
 (add-hook 'dired-mode-hook
           (lambda ()
-            (define-key dired-mode-map (kbd "e") 'dired-xdg-open-file)
-            (dired-omit-mode 1)))
+            (define-key dired-mode-map (kbd "e") 'dired-xdg-open-file)))
+
 
 (setq image-dired-external-viewer "/usr/bin/xdg-open")
 
@@ -65,14 +70,5 @@
 
 (define-key dired-mode-map
   (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
-
-;; Make dired less verbose
-(el-get 'sync 'dired-details)
-(eval-after-load
-    "dired"
-  '(progn
-     (require 'dired-details)
-     (setq-default dired-details-hidden-string "--- ")
-     (dired-details-install)))
 
 ;;; init-dired.el ends here
