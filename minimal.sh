018669aab738692307797a2349e02d7e0ec9ad56
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-04-12 09:35:42 +0900

    * minimal.sh: usage help

--- minimal.sh
+++ minimal.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 # Created: Thursday, March  7 2013
 
-emacs -Q -l ~/.emacs.d/minimal.el -eval "(minimal-load-$1)"
+function usage()
+{
+    cat <<EOF
+
+USAGE:
+       $0 <package-to-load>
+EOF
+    exit 2
+}
+
+if [[ $# -eq 0 ]]; then
+    usage
+else
+    emacs -Q -l ~/.emacs.d/minimal.el -eval "(minimal-load-$1)"
+fi
