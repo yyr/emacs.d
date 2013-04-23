76cceee43c7a9215f0696aa484a521d87d50a27f
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-04-23 14:30:13 +0900

    * init-custom-key-set.el: clean unused custom keys

--- init-custom-key-set.el
+++ init-custom-key-set.el
@@ -1,10 +1,7 @@
-;; -----------------------------------------------------------------------------
 ;; Custom Bindings
-;; -----------------------------------------------------------------------------
+;; ---------------
 
 (global-set-key (kbd "M-SPC") 'set-mark-command) ; was just-one-space
-;; (global-set-key (kbd "M-a") execute-extended-command) ; was backward-sentence
-
 
 ;;; C-M \ (indent) ;; M-; (comment & uncomment-region)
 (global-set-key (kbd "C-c s") 'ispell-word)
@@ -21,12 +18,7 @@
 (global-set-key (kbd "<f9> S") 'ielm)
 (global-set-key (kbd "<f9> f") 'boxquote-insert-file)
 (global-set-key (kbd "<f9> b") 'bbdb)
-(global-set-key (kbd "<f9> r") 'boxquote-region)
 (global-set-key (kbd "<f9> v") 'visible-mode)
-
-
-;; (global-set-key (kbd "C-<f9>") 'previous-buffer)
-;; (global-set-key (kbd "C-<f10>") 'next-buffer)
 
 (defun change-to-other-buffer ()
   "Change to other buffer"
@@ -47,9 +39,8 @@
 
 (global-set-key (kbd "C-s") 'isearch-forward)
 (global-set-key (kbd "C-r") 'isearch-backward)
-
 (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
-(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+
 (global-set-key (kbd "C-x f") 'ffap)
 (global-set-key (kbd "C-x C-p") 'find-file-at-point)
 (global-set-key (kbd "C-c y") 'bury-buffer)
@@ -59,10 +50,7 @@
                                 (message "buffer is reverted")))
 
 (global-set-key (kbd "M-`") 'file-cache-minibuffer-complete)
-
-;; (global-set-key (kbd "C-x C-m") 'execute-extended-command)
-(global-set-key (kbd "C-h a") 'apropos) ;
-;; (global-unset-key "\C-x\C-c")           ;unset kill Emacs
+(global-set-key (kbd "C-h a") 'apropos)
 
 (define-key isearch-mode-map (kbd "C-o")
   (lambda () (interactive)
