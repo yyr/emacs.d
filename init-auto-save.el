14315842f42def3b2f1cd23f9414931ff0bb6576
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-04-23 14:22:31 +0900

    * init-auto-save.el: backup on each save

--- init-auto-save.el
+++ init-auto-save.el
@@ -1,5 +1,7 @@
 ;;; init-auto-save.el
 
+(el-get 'sync 'backup-each-save)
+
 (setq auto-save-interval 200)
 (setq auto-save-timeout 40)
 ;; (setq auto-save-visited-file-name t)
@@ -14,4 +16,21 @@
  kept-old-versions 2
  version-control t) ; use versioned backups
 
+(require 'backup-each-save)
+
+(add-hook 'after-save-hook 'backup-each-save)
+
+(defun backup-each-save-filter (filename)
+  (let ((ignored-filenames
+         '("^/tmp" "semantic.cache$" "\\.emacs-places$"
+           "\\.recentf$" ".newsrc\\(\\.eld\\)?"))
+        (matched-ignored-filename nil))
+    (mapc
+     (lambda (x)
+       (when (string-match x filename)
+         (setq matched-ignored-filename t)))
+     ignored-filenames)
+    (not matched-ignored-filename)))
+
+(setq backup-each-save-filter-function 'backup-each-save-filter)
 ;;; init-auto-save.el ends here
