0d2a1ba1c70d66e129301be4f5008b31f532e63f
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-04-27 15:37:42 +0900

    Seems time has come to get acquainted with Evil.
    * init-evil.el: new

---
+++ init-evil.el
@@ -0,0 +1,9 @@
+;;; init-evil.el
+;;
+;; Author: Yagnesh Raghava Yakkala. http://yagnesh.org
+;;    File: init-evil.el
+;; Created: Friday, April 26 2013
+
+(el-get 'sync 'evil)
+
+;;; init-evil.el ends here
