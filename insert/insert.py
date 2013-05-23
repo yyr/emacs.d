7e22d653c265cef7ea8b34aaf2b67ac0e412d180
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-05-23 12:09:40 +0900

    * insert/insert.py: fix style

--- insert/insert.py
+++ insert/insert.py
@@ -6,11 +6,8 @@
 DATE = "(>>DATE<<)"
 AUTHOR = "Yagnesh Raghava Yakkala"
 WEBSITE = "http://yagnesh.org"
-LICENSE ="GPL v3 or later"
-
-
-
+LICENSE = "GPL v3 or later"
 
 
 if __name__ == '__main__':
-    main()
+    pass
