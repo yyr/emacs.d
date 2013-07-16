b979440f9dc89f297c91902cc9ad88a4436e4aa5
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-07-16 10:30:58 +0900

    * init-flycheck.el: make flycheck less annoying

--- init-flycheck.el
+++ init-flycheck.el
@@ -4,9 +4,13 @@
 
 (after 'flycheck
        (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
-       (setq flycheck-idle-change-timer 3)
+       (setq flycheck-idle-change-delay 10)
+       ;; Highlight whole line with error
+       (setq-default flycheck-highlighting-mode 'columns)
        (setq flycheck-check-syntax-automatically
-             (delq 'new-line flycheck-check-syntax-automatically)))
+             (delq 'new-line flycheck-check-syntax-automatically)
+             ;; '(save idle-change new-line mode-enabled)
+             ))
 
 ;; (add-hook 'prog-mode-hook 'flycheck-mode)
 (dolist (hook '(emacs-lisp-mode-hook
@@ -20,8 +24,6 @@
 
 ;;; for c & cpp
 (after 'flycheck
-       ;; Highlight whole line with error
-       (setq-default flycheck-highlighting-mode 'lines)
        ;; Define a poor c/c++ checker (it fails when errors affect other files,
        ;; not the one being being checked actually)
        (defmacro flycheck-define-clike-checker (name command modes)
@@ -42,5 +44,19 @@
                                       ("g++" "-fsyntax-only" "-Wall" "-Wextra")
                                       c++-mode)
        (add-to-list 'flycheck-checkers 'flycheck-checker-c++))
+
+(defun flycheck-show-nearby-error (arg)
+  "Go forward to next error, display error. if ARG is given go to previous error"
+  (interactive "P")
+  (push-mark)
+  (if arg
+      (flycheck-previous-error)
+    (flycheck-next-error))
+  (let ((errors (flycheck-overlay-errors-at (point))))
+    (if errors
+        (flycheck-display-errors errors)
+      (message "No flycheck errors at this point."))))
+
+(global-set-key (kbd "C-?") 'flycheck-show-nearby-error)
 
 ;;; init-flycheck.el ends here
