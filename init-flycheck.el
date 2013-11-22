c787a25162902121fd96994899d7465585bbf582
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-11-22 15:41:24 +0900

    remove flycheck hack

--- init-flycheck.el
+++ init-flycheck.el
@@ -23,28 +23,6 @@
             (setq flycheck-checker 'bash)))
 
 ;;; for c & cpp
-(after 'flycheck
-       ;; Define a poor c/c++ checker (it fails when errors affect other files,
-       ;; not the one being being checked actually)
-       (defmacro flycheck-define-clike-checker (name command modes)
-         `(flycheck-declare-checker ,(intern (format "flycheck-checker-%s" name))
-            ,(format "A %s checker using %s" name (car command))
-            :command '(,@command source-inplace)
-            :error-patterns
-            '(("^\\(?1:.*\\):\\(?2:[0-9]+\\):\\(?3:[0-9]+\\): error: \\(?4:.*\\)$"
-               error)
-              ("^\\(?1:.*\\):\\(?2:[0-9]+\\):\\(?3:[0-9]+\\): warning: \\(?4:.*\\)$"
-               warning))
-            :modes ',modes))
-       (flycheck-define-clike-checker c
-                                      ("gcc" "-fsyntax-only" "-Wall" "-Wextra")
-                                      c-mode)
-       (add-to-list 'flycheck-checkers 'flycheck-checker-c)
-       (flycheck-define-clike-checker c++
-                                      ("g++" "-fsyntax-only" "-Wall" "-Wextra")
-                                      c++-mode)
-       (add-to-list 'flycheck-checkers 'flycheck-checker-c++))
-
 (defun flycheck-show-nearby-error (arg)
   "Go forward to next error, display error. if ARG is given go to previous error"
   (interactive "P")
