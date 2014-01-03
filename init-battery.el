77eb1a71f35b65866d5584f300c5f06e9be9f679
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-01-04 00:42:06 +0900

    * init-battery.el: indentation fix.

--- init-battery.el
+++ init-battery.el
@@ -7,9 +7,11 @@
 ;;; http://stackoverflow.com/questions/5562974/emacs-is-it-running-on-laptop-or-on-desktop
 (require 'battery)
 (when (and battery-status-function
-           (not (string-match-p "N/A"
-                                (battery-format "%B"
-                                                (funcall battery-status-function)))))
+           (not (string-match-p
+                 "N/A"
+                 (battery-format
+                  "%B"
+                  (funcall battery-status-function)))))
   (display-battery-mode 1))
 
 ;;; init-battery.el ends here
