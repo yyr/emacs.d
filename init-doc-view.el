561b9f805e0b67e1a6b20c75b105ee9f6e993634
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-04-28 04:15:09 +0900

    * init-doc-view.el: extension.

---
+++ init-doc-view.el
@@ -0,0 +1,11 @@
+;;; init-doc-view.el
+;; Author: Yagnesh Raghava Yakkala. http://yagnesh.org
+;;    File: init-doc-view.el
+;; Created: Monday, March 31 2014
+
+;;; docview.
+(add-hook 'doc-view-mode-hook
+          (lambda ()
+            (local-set-key "G" 'doc-view-goto-page)))
+
+;;; init-doc-view.el ends here
