860beba12ae5cd598ee4bdc1c324291c5f934096
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-08-20 21:28:29 +0900

    * lisp/init-misc.el: cleanup

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -43,22 +43,6 @@
   (interactive)
   (load-file "~/.emacs.d/init.el"))
 
-;; zap-up-to-char
-;; --------------
-;; Most of the time when I want to zap-to-char, it's to a delimeter that
-;; I'd like to be left alone rather than zapped too.
-;;
-(defun zap-up-to-char (arg char)
-  "Like standard zap-to-char, but stops just before the given character."
-  (interactive "p\ncZap up to char: ")
-  (kill-region (point)
-               (progn
-                 (search-forward (char-to-string char) nil nil arg)
-                 (forward-char (if (>= arg 0) -1 1))
-                 (point))))
-(define-key global-map [(meta ?z)] 'zap-up-to-char)           ; Rebind M-z to our version
-
-
 ;; goto-matching-paren
 ;; -------------------
 ;; If point is sitting on a parenthetic character, jump to its match.
