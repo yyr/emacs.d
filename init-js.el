864ac80de1376dfdb80e54e3010f78f336f0f28d
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-08-25 01:03:19 +0900

    init-js.el: new

---
+++ init-js.el
@@ -0,0 +1,12 @@
+;;; init-js.el
+;;
+;; Author: Yagnesh Raghava Yakkala. http://yagnesh.org
+;;    File: init-js.el
+;; Created: Thursday, November 21 2013
+
+;;; Description:
+;;
+
+(el-get 'sync '(js2-mode web-mode))
+
+;;; init-js.el ends here
