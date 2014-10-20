1f309685f46a30ad44f87c186fc96449e2057875
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-10-20 14:25:09 +0900

    * lisp/init-buffers.el (undo-kill-buffer): new function, bind to M-f9

--- lisp/init-buffers.el
+++ lisp/init-buffers.el
@@ -138,4 +138,24 @@ FIXME: make an exceptions for few buffers like gnus, emms"
                               (interactive)
                               (kill-buffer (buffer-name))))
 
+;;; From Emacswiki http://www.emacswiki.org/RecentFiles
+(defun undo-kill-buffer (arg)
+  "Re-open the last buffer killed.  With ARG, re-open the nth buffer."
+  (interactive "p")
+  (let ((recently-killed-list (copy-sequence recentf-list))
+        (buffer-files-list
+         (delq nil (mapcar (lambda (buf)
+                             (when (buffer-file-name buf)
+                               (expand-file-name (buffer-file-name buf)))) (buffer-list)))))
+    (mapc
+     (lambda (buf-file)
+       (setq recently-killed-list
+	     (delq buf-file recently-killed-list)))
+     buffer-files-list)
+    (find-file
+     (if arg (nth arg recently-killed-list)
+       (car recently-killed-list)))))
+
+(global-set-key (kbd "M-<f9>") #'undo-kill-buffer)
+
 ;;; init-buffers.el ends here
