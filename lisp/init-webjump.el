83e3e2a3b155f38af461d895357f984fdc07d507
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-10-24 15:33:38 +0900

    * lisp/init-webjump.el: remove require. add keybinding.

--- lisp/init-webjump.el
+++ lisp/init-webjump.el
@@ -3,11 +3,10 @@
 ;; Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
 ;; License: GPL v3 or later
 
-(require 'webjump)
 (setq webjump-sites
       (append '(("yorg" . "yagnesh.org")
                 ("sap" . "sapporoindians.net"))
               webjump-sample-sites))
-;; (global-set-key "\C-cj" 'webjump)
+(global-set-key "\C-cj" 'webjump)
 
 ;;; init-webjump.el ends here
