887da0f7ef285bf7af4ae7bc3cda5647fa9383fe
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-10-29 12:06:43 +0900

    lisp/init-smex.el: Remove duplicate code. el-get already does that

--- lisp/init-smex.el
+++ lisp/init-smex.el
@@ -8,8 +8,6 @@
 ;; -----------------------------------------------------------------------------
 (el-get 'sync '(smex))
 
-(require 'smex)
-(smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
