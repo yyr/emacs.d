f50eaf146543d1e017d18cdbd78848852b3882fb
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-15 18:57:54 +0900

    more cleanup

--- insert/insert.el
+++ insert/insert.el
@@ -1,11 +1,7 @@
 ;;; (>>FILE<<)
-;;
 ;; Author: Yagnesh Raghava Yakkala. http://yagnesh.org
-;;    File: (>>FILE<<)
 ;; Created: (>>DATE<<)
 
-;;; Description:
-;;
 
 
 ;;; (>>FILE<<) ends here
--- insert/insert.py
+++ insert/insert.py
@@ -3,10 +3,7 @@
 
 '''
 
-DATE = "(>>DATE<<)"
-AUTHOR = "Yagnesh Raghava Yakkala"
-WEBSITE = "http://yagnesh.org"
-LICENSE = "GPL v3 or later"
+
 
 
 if __name__ == '__main__':
