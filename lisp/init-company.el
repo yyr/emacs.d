34beaa1a1fa7632aa07e5514271cb72a1f0cf11d
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-16 10:51:34 +0900

    * lisp/init-company.el: require company.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -3,8 +3,9 @@
 ;; Created: Thursday, January 15 2015
 
 (el-get  'sync  'company-mode)
-(add-hook 'after-init-hook 'global-company-mode)
+(require 'company)
 
+(add-hook 'after-init-hook 'global-company-mode)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
 (define-key company-active-map  "\t"    'company-complete-common)
