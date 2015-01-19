fe696999bc4829bfc9e22c5ee608f0d69f17971e
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-19 15:48:40 +0900

    cleanup

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -22,23 +22,7 @@
                 sh-mode-hook))
   (add-hook hook 'flycheck-mode))
 
-;;; for c & cpp
-(defun flycheck-show-nearby-error (arg)
-  "Go forward to next error, display error. if ARG is given go to previous error"
-  (interactive "P")
-  (push-mark)
-  (if arg
-      (flycheck-previous-error)
-    (flycheck-next-error))
-  (let ((errors (flycheck-overlay-errors-at (point))))
-    (if errors
-        (flycheck-display-errors errors)
-      (message "No further errors from this point."))))
-
-(global-set-key (kbd "C-?") 'flycheck-show-nearby-error)
-
-
-;;; elisp
+;; elisp
 (setq-default flycheck-emacs-lisp-load-path load-path)
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
