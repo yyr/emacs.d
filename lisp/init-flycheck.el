9a307b8aed58222bebac7aec3ac271f0aa218ee7
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-19 11:18:45 +0900

    * lisp/init-flycheck.el: better flycheck config

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,16 +1,17 @@
 ;;; init-flycheck.el
-
 (el-get 'sync 'flycheck)
+(add-hook 'prog-mode 'flycheck-mode)
+(add-hook 'latex-mode 'flycheck-mode)
+
+(global-set-key "\M-o" flycheck-keymap-prefix)
 
 (after 'flycheck
-       (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
-       (setq flycheck-idle-change-delay 10)
+  (setq flycheck-idle-change-delay 10)
        ;; Highlight whole line with error
        (setq flycheck-highlighting-mode 'columns)
        (setq flycheck-check-syntax-automatically
-             (delq 'new-line flycheck-check-syntax-automatically)
              ;; '(save idle-change new-line mode-enabled)
-             ))
+             (delq 'new-line flycheck-check-syntax-automatically)))
 
 ;; (add-hook 'prog-mode-hook 'flycheck-mode)
 (dolist (hook '(emacs-lisp-mode-hook
@@ -29,8 +30,18 @@
   (let ((errors (flycheck-overlay-errors-at (point))))
     (if errors
         (flycheck-display-errors errors)
-      (message "No flycheck errors at this point."))))
+      (message "No further errors from this point."))))
 
 (global-set-key (kbd "C-?") 'flycheck-show-nearby-error)
+
+
+;;; elisp
+(setq-default flycheck-emacs-lisp-load-path load-path)
+(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+
+;;; python
+;; (setq-default flycheck-disabled-checkers (append '(python-pylint)
+;;                                                  flycheck-disabled-checkers))
+
 
 ;;; init-flycheck.el ends here
