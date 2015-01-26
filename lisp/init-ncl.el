2a9b95224f23fc0b71b3a73cb81329ad83dc6231
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-26 16:10:39 +0900

    cleanup ncl-mode loading

--- lisp/init-ncl.el
+++ lisp/init-ncl.el
@@ -1,7 +1,6 @@
 ;;; init-ncl.el
 
 (el-get 'sync '(ncl-mode))
-(load "~/.emacs.d/el-get/ncl-mode/ncl-mode-load.el")
 
 ;; use downloaded documentation instead (for offline use)
 ;; (setq ncl-doc-url-base (directory-file-name "file:///home/yagnesh/docs/ncl"))
