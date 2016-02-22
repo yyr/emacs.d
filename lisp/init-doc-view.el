fafe39db11032e4e11f0bf18cd3e2a656dadbd63
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2016-02-22 16:18:27 +0530

    move pdf-tools init-latex.el

--- lisp/init-doc-view.el
+++ lisp/init-doc-view.el
@@ -7,13 +7,5 @@
           (lambda ()
             (local-set-key "G" 'doc-view-goto-page)))
 
-(when (and (not on-lab-server) (not on-ms))
-  (progn
-    (el-get 'sync 'pdf-tools)
-    (pdf-tools-install)
-    (loop for key in
-          '(("G" pdf-view-goto-page)
-            )
-          do (define-key pdf-view-mode-map (car key) (cadr key)))))
 
 ;;; init-doc-view.el ends here
