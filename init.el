e25638a0fb3f1c52281a52c3ec005ed5753fa5af
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2018-01-29 08:06:16 +0530

    * init.el: use load! (macro for load-file) instead of load

--- init.el
+++ init.el
@@ -69,6 +69,9 @@ mark a check point"
   (interactive)
   (load-file "~/.emacs.d/init.el"))
 
+(defmacro load! (fname)
+  (load-file (expand-file-name (concat fname ".el") emacsd)))
+
 ;; ----------------------------------------------------------------------------
 ;;; load individual configuration files
 ;; ----------------------------------------------------------------------------
@@ -78,144 +81,144 @@ mark a check point"
 
 ;;; first of all any OS dependent paths has to be set
 (when on-ms
-  (load "init-ms"))
+  (load! "init-ms"))
 
 ;;; package managers
-(load "init-el-get")
+(load! "init-el-get")
 
 ;;; version control
-(load "init-git")
+(load! "init-git")
 
 ;;; IDE , make
-;; (load "init-cedet")
+;; (load! "init-cedet")
 
 ;;; mail, news & messages
-(load "init-gpg")
-(load "init-pre-gnus")
-(load "init-mail")
-(load "init-message")
+(load! "init-gpg")
+(load! "init-pre-gnus")
+(load! "init-mail")
+(load! "init-message")
 
 ;;; initial setup, keybindings
-(load "init-general")
-(load "init-custom-key-set")
-(load "init-minors")
-(load "init-auto-save")
+(load! "init-general")
+(load! "init-custom-key-set")
+(load! "init-minors")
+(load! "init-auto-save")
 
 ;;; make friendly mini buffer
-(load "init-ido")
+(load! "init-ido")
 
 ;;; help typing
-(load "init-auto-fill")
-(load "init-auto-insert")
-(load "init-flyspell")
-(load "init-hippie-expand")
-;; (load "init-auto-complete")
-(load "init-company")
+(load! "init-auto-fill")
+(load! "init-auto-insert")
+(load! "init-flyspell")
+(load! "init-hippie-expand")
+;; (load! "init-auto-complete")
+(load! "init-company")
 
-(load "init-yasnip")
-(load "init-multiple-cursors")
-(load "init-dev")
+(load! "init-yasnip")
+(load! "init-multiple-cursors")
+(load! "init-dev")
 
 ;;; look
-(load "init-font")
-(load "init-point")
+(load! "init-font")
+(load! "init-point")
 
 ;; dictionary
-(when (executable-find "dict") (load "init-dictem"))
-(when (file-directory-p "~/git/sdic") (load "init-sdic"))
-(load "init-edict")
-;; (when (not on-ms) (load "init-japanese"))
+(when (executable-find "dict") (load! "init-dictem"))
+(when (file-directory-p "~/git/sdic") (load! "init-sdic"))
+(load! "init-edict")
+;; (when (not on-ms) (load! "init-japanese"))
 
 ;;; help navigation
-(load "init-navigation")
-(load "init-editing")
-(load "init-imenu")
-(load "init-buffers")
-(load "init-recentf")
+(load! "init-navigation")
+(load! "init-editing")
+(load! "init-imenu")
+(load! "init-buffers")
+(load! "init-recentf")
 
 ;;; shell, term, comint, tramp
-(load "init-term")
-(load "init-tramp")
-(load "init-unixutils")
+(load! "init-term")
+(load! "init-tramp")
+(load! "init-unixutils")
 
 ;;; window, frame
-(load "init-windows")
-(load "init-frame")
+(load! "init-windows")
+(load! "init-frame")
 
 ;;; File management
-(load "init-dired")
+(load! "init-dired")
 
 ;;; emacs session
-;;(load "init-session")
+;;(load! "init-session")
 
 ;;; languages
-(load "init-lispy")
-(load "init-elisp")
-(load "init-common-lisp")
-(load "init-scheme")
-(load "init-grammar")
+(load! "init-lispy")
+(load! "init-elisp")
+(load! "init-common-lisp")
+(load! "init-scheme")
+(load! "init-grammar")
 
-(load "init-cc")
-(load "init-perl")
-(load "init-html")
-(load "init-yaml")
-(load "init-python")
-(load "init-compile")
+(load! "init-cc")
+(load! "init-perl")
+(load! "init-html")
+(load! "init-yaml")
+(load! "init-python")
+(load! "init-compile")
 
-(load "init-fortran")
+(load! "init-fortran")
 
 ;;; code assistance
-(load "init-flycheck")
-(load "init-etags")
+(load! "init-flycheck")
+(load! "init-etags")
 
 ;;; subject
-(load "init-ncl")
-(load "init-ferret")
+(load! "init-ncl")
+(load! "init-ferret")
 (when (and (not on-lab-server) (not on-ms))
-  (load "init-latex")
-  (load "init-bib"))
-(load "init-doc-view")
+  (load! "init-latex")
+  (load! "init-bib"))
+(load! "init-doc-view")
 
 ;;; ORG
-(load "init-org")
-(load "init-org-todo-state-tags")
-(load "init-org-agenda")
-(load "init-org-tags")
-(load "init-org-capture")
-(load "init-org-ob")
-(load "init-org-diary")
-(load "init-calender")
-(load "init-weather-metno")
+(load! "init-org")
+(load! "init-org-todo-state-tags")
+(load! "init-org-agenda")
+(load! "init-org-tags")
+(load! "init-org-capture")
+(load! "init-org-ob")
+(load! "init-org-diary")
+(load! "init-calender")
+(load! "init-weather-metno")
 
 ;;; org publish
-(load "init-org-export")
-(load "init-org-misc")
+(load! "init-org-export")
+(load! "init-org-misc")
 
 ;;; info,  help, doc, man(woman) etc..
-(load "init-info")
-(load "init-eldoc")
-(load "init-help")
-(load "init-outline")
+(load! "init-info")
+(load! "init-eldoc")
+(load! "init-help")
+(load! "init-outline")
 
 ;;; web
-(load "init-web")
-;; (load "init-erc")
+(load! "init-web")
+;; (load! "init-erc")
 
 ;;;  misc
-(load "init-misc")
-(load "init-battery")
-(load "init-utils")
-(load "init-boxquote")
-;; (when (and (not on-lab-server) (not on-ms)) (load "init-social"))
+(load! "init-misc")
+(load! "init-battery")
+(load! "init-utils")
+(load! "init-boxquote")
+;; (when (and (not on-lab-server) (not on-ms)) (load! "init-social"))
 
 ;;; sys & server
-(load "init-print")
-(load "init-crontab")
-(load "init-server")
-(load "init-time")
+(load! "init-print")
+(load! "init-crontab")
+(load! "init-server")
+(load! "init-time")
 
 ;;; music
-;; (when (and (not on-lab-server) (not on-ms)) (load "init-emms"))
+;; (when (and (not on-lab-server) (not on-ms)) (load! "init-emms"))
 
-(load "init-finish")
+(load! "init-finish")
 ;; init ends here
