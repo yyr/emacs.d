326ce984d22f4bcfe424b2d94aafb93513e4d745
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2018-01-30 04:43:05 +0530

    * lisp/init-battery.el: deffer display-battery-mode after startup.

--- lisp/init-battery.el
+++ lisp/init-battery.el
@@ -2,13 +2,15 @@
 ;; Author: Yagnesh Raghava Yakkala. http://yagnesh.org
 
 ;;; http://stackoverflow.com/questions/5562974/emacs-is-it-running-on-laptop-or-on-desktop
-(require 'battery)
-(when (and battery-status-function
-           (not (string-match-p
-                 "N/A"
-                 (battery-format
-                  "%B"
-                  (funcall battery-status-function)))))
-  (display-battery-mode 1))
+;; (require 'battery)
+;; (when (and battery-status-function
+;;            (not (string-match-p
+;;                  "N/A"
+;;                  (battery-format
+;;                   "%B"
+;;                   (funcall battery-status-function)))))
+;;   (display-battery-mode 1))
 
+(add-hook 'emacs-startup-hook
+          '(lambda nil (display-battery-mode 1)))
 ;;; init-battery.el ends here
