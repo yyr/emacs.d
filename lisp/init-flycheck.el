b7cf28837f02841186c31fdfa015c931789d40f8
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2018-01-30 05:59:55 +0530

    * lisp/init-flycheck.el: cleanup, remove require.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,16 +1,20 @@
 ;;; init-flycheck.el
 (el-get 'sync 'flycheck)
-(require 'flycheck)
-(add-hook 'prog-mode 'flycheck-mode)
-(add-hook 'latex-mode 'flycheck-mode)
+(add-hook 'prog-mode-hook 'flycheck-mode)
+;; (dolist (hook '(emacs-lisp-mode-hook
+;;                 c-mode-hook
+;;                 python-mode-hook
+;;                 sh-mode-hook))
+;;   (add-hook hook 'flycheck-mode))
+;; (add-hook 'latex-mode 'flycheck-mode)
 
 (global-unset-key (kbd "M-o"))         ; stupid prefix I never used in my life.
-(define-key flycheck-mode-map flycheck-keymap-prefix nil)
 (setq flycheck-keymap-prefix [(meta ?o)])
-(define-key flycheck-mode-map flycheck-keymap-prefix
-  flycheck-command-map)
 
 (after 'flycheck
+  (define-key flycheck-mode-map flycheck-keymap-prefix nil)
+  (define-key flycheck-mode-map flycheck-keymap-prefix
+    flycheck-command-map)
   (setq flycheck-idle-change-delay 10)
   ;; Highlight whole line with error
   (setq flycheck-highlighting-mode 'columns)
@@ -18,12 +22,6 @@
         ;; '(save idle-change new-line mode-enabled)
         (delq 'new-line flycheck-check-syntax-automatically)))
 
-;; (add-hook 'prog-mode-hook 'flycheck-mode)
-(dolist (hook '(emacs-lisp-mode-hook
-                c-mode-hook
-                python-mode-hook
-                sh-mode-hook))
-  (add-hook hook 'flycheck-mode))
 
 ;; elisp
 (setq-default flycheck-emacs-lisp-load-path load-path)
@@ -32,6 +30,5 @@
 ;;; python
 ;; (setq-default flycheck-disabled-checkers (append '(python-pylint)
 ;;                                                  flycheck-disabled-checkers))
-
 
 ;;; init-flycheck.el ends here
